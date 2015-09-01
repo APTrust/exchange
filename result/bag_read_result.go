3bef878bfa492a07b309e0d97f91ad72a84c260a
A. Diamond <diamond@sheprador.com>
2015-09-01 16:00:36 -0400

    Moved some functions in bag_read_result

--- result/bag_read_result.go
+++ result/bag_read_result.go
@@ -59,6 +59,41 @@ func (result *BagReadResult) TagValue(tagLabel string) (tagValue string) {
 	return tagValue
 }
 
+// Returns the bag's access rights, which should be one of
+// "consortia", "institution" or "restricted" (all lower-case,
+// to comply with the needs of our Fluctus REST service).
+func (result *BagReadResult) Access() string {
+	accessRights := result.TagValue("Access")
+	if accessRights == "" {
+		// Some older bags may have the deprecated Rights tag
+		accessRights = result.TagValue("Rights")
+	}
+	return strings.ToLower(accessRights)
+}
+
+// Returns the title of the bag, which is extracted from
+// the tag files.
+func (result *BagReadResult) Title() string {
+	return result.TagValue("Title")
+}
+
+// Returns the description of the bag, which is extracted from
+// the tag files.
+func (result *BagReadResult) Description() string {
+	return result.TagValue("Internal-Sender-Description")
+}
+
+// Returns the alternate ID of the bag, which is extracted from
+// the tag files. Note that this is a slice containing a single
+// string, because Fedora expects an array of one or more
+// alternate identifiers.
+func (result *BagReadResult) AltId() []string {
+	altId := make([]string, 1)
+	altId[0] = result.TagValue("Internal-Sender-Identifier")
+	return altId
+}
+
+
 // Result Interface functions
 
 func (result *BagReadResult) Succeeded() bool {
--- result/bag_read_result_test.go
+++ result/bag_read_result_test.go
@@ -24,3 +24,85 @@ func TestTagValue(t *testing.T) {
 		t.Error("TagValue returned wrong result.")
 	}
 }
+
+func TestAccess(t *testing.T) {
+	bagReadResult := &result.BagReadResult{}
+	bagReadResult.Tags = make([]result.Tag, 1)
+	bagReadResult.Tags[0] = result.Tag{
+		Label: "Access",
+		Value: "consortia",
+	}
+
+	if bagReadResult.Access() != "consortia" {
+		t.Error("Access() should have returned consortia")
+	}
+
+	// Test reading of deprecated Rights tag.
+	bagReadResult.Tags[0] = result.Tag{
+		Label: "Rights",
+		Value: "institution",
+	}
+	if bagReadResult.Access() != "institution" {
+		t.Error("Access() should have returned institution")
+	}
+
+	bagReadResult.Tags[0] = result.Tag{Label: "X", Value: "Y"}
+	if bagReadResult.Access() != "" {
+		t.Error("Access() should have returned empty string")
+	}
+}
+
+func TestTitle(t *testing.T) {
+	bagReadResult := &result.BagReadResult{}
+	bagReadResult.Tags = make([]result.Tag, 1)
+	bagReadResult.Tags[0] = result.Tag{
+		Label: "Title",
+		Value: "Hang on Sloopy",
+	}
+
+	if bagReadResult.Title() != "Hang on Sloopy" {
+		t.Error("Title() should have returned an old song title")
+	}
+
+	bagReadResult.Tags[0] = result.Tag{Label: "X", Value: "Y"}
+	if bagReadResult.Title() != "" {
+		t.Error("Title() is just making stuff up")
+	}
+}
+
+func TestDescription(t *testing.T) {
+	bagReadResult := &result.BagReadResult{}
+	bagReadResult.Tags = make([]result.Tag, 1)
+	bagReadResult.Tags[0] = result.Tag{
+		Label: "Internal-Sender-Description",
+		Value: "Loud and obtuse",
+	}
+
+	if bagReadResult.Description() != "Loud and obtuse" {
+		t.Error("Description() should have returned 'Loud and obtuse'")
+	}
+
+	bagReadResult.Tags[0] = result.Tag{Label: "X", Value: "Y"}
+	if bagReadResult.Description() != "" {
+		t.Error("Description() is just making stuff up")
+	}
+}
+
+func TestAltId(t *testing.T) {
+	bagReadResult := &result.BagReadResult{}
+	bagReadResult.Tags = make([]result.Tag, 1)
+	bagReadResult.Tags[0] = result.Tag{
+		Label: "Internal-Sender-Identifier",
+		Value: "abc-123",
+	}
+	expected := make([]string, 1)
+	expected[0] = "abc-123"
+	if bagReadResult.AltId()[0] != "abc-123" {
+		t.Error("AltId() should have returned 'abc-123'")
+	}
+
+	bagReadResult.Tags[0] = result.Tag{Label: "X", Value: "Y"}
+	if bagReadResult.AltId()[0] != "" {
+		t.Error("AltId() is just making stuff up")
+	}
+}
