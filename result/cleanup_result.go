6ac5f3e4485da470eda812b072e18d9e09ac8606
A. Diamond <diamond@sheprador.com>
2015-09-03 14:38:41 -0400

    Added cleanup result with tests

---
+++ result/cleanup_result.go
@@ -0,0 +1,48 @@
+package result
+
+import (
+	"github.com/APTrust/exchange/models"
+	"time"
+)
+
+// CleanupResult describes one or more files to be
+// deleted from S3, and the result of the attempts
+// to delete those files. The combination of BagName +
+// ETag + BagDate maps to a unique entry in Fluctus'
+// ProcessedItems table.
+type CleanupResult struct {
+	// The S3 key of the original bag file. This will
+	// be in one of the receiving buckets. This is not
+	// necessarily the file we'll be deleting, but all
+	// files to be deleted are related to this bag.
+	BagName            string
+	// The ETag of the original uploaded bag (minus the
+	// quotes). This is the bag's md5 sum for bags under
+	// about 2GB.
+	ETag               string
+	// The modified date of the original bag.
+	BagDate            time.Time
+	// The identifier of the intellectual object to which
+	// the Files belong. This may be an empty string in
+	// cases where we're cleaning up files from a bag that
+	// failed ingest. If it's not null, the bag was successfully
+	// ingested, and the identifier will look something like
+	// virginia.edu/bag_name
+	ObjectIdentifier   string
+	// Files contains a list of files/keys to be deleted
+	// from S3.
+	Files              []*models.S3File
+}
+
+// Returns true if all files were successfully deleted.
+func (result *CleanupResult) Succeeded() bool {
+	for _, file := range result.Files {
+		if file.DeleteSkippedPerConfig == true {
+			continue
+		}
+		if file.DeleteAttempted() == false || file.DeletedAt.IsZero() == true {
+			return false
+		}
+	}
+	return true
+}
---
+++ result/cleanup_result_test.go
@@ -0,0 +1,57 @@
+package result_test
+
+import (
+	"encoding/json"
+	"github.com/APTrust/exchange/result"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestDeleteAttemptedAndSucceeded(t *testing.T) {
+	// TODO: Need a more reliable way to get path to test data file
+	filepath := filepath.Join("..", "testdata", "cleanup_result.json")
+	var result result.CleanupResult
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		t.Errorf("Error loading cleanup result test file '%s': %v", filepath, err)
+	}
+	err = json.Unmarshal(file, &result)
+	if err != nil {
+		t.Errorf("Error loading cleanup result test file '%s': %v", filepath, err)
+	}
+
+	if result.Succeeded() == false {
+		t.Error("result.Succeeded() should have returned true")
+	}
+	for _, file := range result.Files {
+		if file.DeleteAttempted() == false {
+			t.Error("file.DeleteAttempted() should have returned true")
+		}
+		// Set these for next test
+		file.DeletedAt = time.Time{}
+		file.ErrorMessage = "Spongebob"
+	}
+
+	if result.Succeeded() == true {
+		t.Error("result.Succeeded() should have returned false")
+	}
+	for _, file := range result.Files {
+		if file.DeleteAttempted() == false {
+			t.Error("file.DeleteAttempted() should have returned true")
+		}
+		// Set these for next test
+		file.DeletedAt = time.Time{}
+		file.ErrorMessage = ""
+	}
+
+	if result.Succeeded() == true {
+		t.Error("result.Succeeded() should have returned false")
+	}
+	for _, file := range result.Files {
+		if file.DeleteAttempted() == true {
+			t.Error("file.DeleteAttempted() should have returned false")
+		}
+	}
+}
