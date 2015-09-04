3bcefd890599441de19a6ab67fddd573190bf1ff
A. Diamond <diamond@sheprador.com>
2015-09-04 15:53:35 -0400

    Added FixityResult

---
+++ result/fixity_result.go
@@ -0,0 +1,124 @@
+package result
+
+import (
+	"fmt"
+	"github.com/APTrust/exchange/models"
+	"github.com/nu7hatch/gouuid"
+	"time"
+	"strings"
+)
+
+
+// FixityResult descibes the results of fetching a file from S3
+// and verification of the file's sha256 checksum.
+type FixityResult struct {
+
+	// The generic file we're going to look at.
+	// This file is sitting somewhere on S3.
+	GenericFile   *models.GenericFile
+
+	// Does the file exist in S3?
+	S3FileExists  bool
+
+	// The sha256 sum we calculated after downloading
+	// the file.
+	Sha256        string
+
+	// Information about the result of this operation.
+	Result        Result
+}
+
+
+func NewFixityResult(gf *models.GenericFile) (*FixityResult) {
+	return &FixityResult {
+		GenericFile: gf,
+		S3FileExists: true,
+		Result: NewResult(),
+	}
+}
+
+// Returns the name of the S3 bucket and key for the GenericFile.
+func (result *FixityResult) BucketAndKey() (string, string, error) {
+	parts := strings.Split(result.GenericFile.URI, "/")
+	length := len(parts)
+	if length < 4 {
+		// This error is fatal, so don't retry.
+		result.Result.AddError("GenericFile URI '%s' is invalid", result.GenericFile.URI)
+		result.Result.Retry = false
+		return "","", fmt.Errorf(result.Result.Errors[0])
+	}
+	bucket := parts[length - 2]
+	key := parts[length - 1]
+	return bucket, key, nil
+}
+
+// Returns true if result.Sha256 was set.
+func (result *FixityResult) GotDigestFromPreservationFile() (bool) {
+	return result.Sha256 != ""
+}
+
+// Returns true if the underlying GenericFile includes a SHA256 checksum.
+func (result *FixityResult) GenericFileHasDigest() (bool) {
+	return result.FedoraSha256() != ""
+}
+
+// Returns the SHA256 checksum that Fedora has on record.
+func (result *FixityResult) FedoraSha256() (string) {
+	checksum := result.GenericFile.GetChecksum("sha256")
+	if checksum == nil {
+		return ""
+	}
+	return checksum.Digest
+}
+
+// Returns true if we have all the data we need to compare the
+// existing checksum with the checksum of the S3 file.
+func (result *FixityResult) FixityCheckPossible() (bool) {
+	return result.GotDigestFromPreservationFile() && result.GenericFileHasDigest()
+}
+
+// Returns true if the sha256 sum we calculated for this file
+// matches the sha256 sum recorded in Fedora.
+func (result *FixityResult) Sha256Matches() (bool, error) {
+	if result.FixityCheckPossible() == false {
+		return false, fmt.Errorf("Fixity check is not possible because one or more checksums are not available.")
+	}
+	return result.FedoraSha256() == result.Sha256, nil
+}
+
+// Returns a PremisEvent describing the result of this fixity check.
+func (result *FixityResult) BuildPremisEvent() (*models.PremisEvent, error) {
+	detail := "Fixity check against registered hash"
+	outcome := "success"
+	outcomeInformation := "Fixity matches"
+	ok, err := result.Sha256Matches()
+	if err != nil {
+		return nil, err
+	}
+	if ok == false {
+		detail = "Fixity does not match expected value"
+		outcome = "failure"
+		outcomeInformation = fmt.Sprintf("Expected digest '%s', got '%s'",
+			result.FedoraSha256(), result.Sha256)
+	}
+
+	youyoueyedee, err := uuid.NewV4()
+	if err != nil {
+		detailedErr := fmt.Errorf("Error generating UUID for fixity check event: %v", err)
+		return nil, detailedErr
+	}
+
+	premisEvent := &models.PremisEvent {
+		Identifier: youyoueyedee.String(),
+		EventType: "fixity_check",
+		DateTime: time.Now().UTC(),
+		Detail: detail,
+		Outcome: outcome,
+		OutcomeDetail: result.Sha256,
+		Object: "Go language cryptohash",
+		Agent: "http://golang.org/pkg/crypto/sha256/",
+		OutcomeInformation: outcomeInformation,
+	}
+
+	return premisEvent, nil
+}
---
+++ result/fixity_result_test.go
@@ -0,0 +1,230 @@
+package result_test
+
+import (
+	"github.com/APTrust/exchange/result"
+	"github.com/APTrust/exchange/models"
+	"testing"
+	"time"
+)
+
+var md5sum = "1234567890"
+var sha256sum = "fedcba9876543210"
+
+func getGenericFile() (*models.GenericFile) {
+	checksums := make([]*models.ChecksumAttribute, 2)
+	checksums[0] = &models.ChecksumAttribute{
+		Algorithm: "md5",
+		DateTime: time.Date(2014,11,11,12,0,0,0,time.UTC),
+		Digest: md5sum,
+	}
+	checksums[1] = &models.ChecksumAttribute{
+		Algorithm: "sha256",
+		DateTime: time.Date(2014,11,11,12,0,0,0,time.UTC),
+		Digest: sha256sum,
+	}
+	return &models.GenericFile{
+		URI: "https://s3.amazonaws.com/aptrust.preservation.storage/52a928da-89ef-48c6-4627-826d1858349b",
+		ChecksumAttributes: checksums,
+	}
+}
+
+func TestBucketAndKey(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	bucket, key, err := result.BucketAndKey()
+	if err != nil {
+		t.Errorf("BucketAndKey() returned error: %v", err)
+	}
+	if bucket != "aptrust.preservation.storage" {
+		t.Errorf("BucketAndKey() returned bucket name '%s', expected 'aptrust.preservation.storage'", bucket)
+	}
+	if key != "52a928da-89ef-48c6-4627-826d1858349b" {
+		t.Errorf("BucketAndKey() returned key '%s', expected '52a928da-89ef-48c6-4627-826d1858349b'", key)
+	}
+}
+
+func TestBucketAndKeyWithBadUri(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	result.GenericFile.URI = "http://example.com"
+	_, _, err := result.BucketAndKey()
+	if err == nil {
+		t.Errorf("BucketAndKey() should have returned an error for invalid URI")
+		return
+	}
+	if result.Result.Errors[0] != "GenericFile URI 'http://example.com' is invalid" {
+		t.Errorf("BucketAndKey() did not set descriptive error message for bad URI")
+	}
+	if result.Result.Retry == true {
+		t.Errorf("Retry should have been set to false after fatal error.")
+	}
+}
+
+
+func TestSha256Matches(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	result.Sha256 = sha256sum
+	matches, err := result.Sha256Matches()
+	if err != nil {
+		t.Error(err)
+	}
+	if matches == false {
+		t.Errorf("Sha256Matches should have returned true")
+	}
+
+
+	result.Sha256 = "some random string"
+	matches, err = result.Sha256Matches()
+	if err != nil {
+		t.Error(err)
+	}
+	if matches == true {
+		t.Errorf("Sha256Matches should have returned false")
+	}
+}
+
+func TestMissingChecksums(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	_, err := result.Sha256Matches()
+	if err == nil {
+		t.Errorf("Sha256Matches should have returned a usage error")
+	}
+
+	result.Sha256 = sha256sum
+	result.GenericFile.ChecksumAttributes = make([]*models.ChecksumAttribute, 2)
+	_, err = result.Sha256Matches()
+	if err == nil {
+		t.Errorf("Sha256Matches should have returned a usage error")
+	}
+}
+
+func TestGotDigestFromPreservationFile(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	if result.GotDigestFromPreservationFile() == true {
+		t.Errorf("GotDigestFromPreservationFile() should have returned false")
+	}
+	result.Sha256 = sha256sum
+	if result.GotDigestFromPreservationFile() == false {
+		t.Errorf("GotDigestFromPreservationFile() should have returned true")
+	}
+}
+
+func TestGenericFileHasDigest(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	if result.GenericFileHasDigest() == false {
+		t.Errorf("GenericFileHasDigest() should have returned true")
+	}
+	// Make the SHA256 checksum disappear
+	for i := range result.GenericFile.ChecksumAttributes {
+		result.GenericFile.ChecksumAttributes[i].Algorithm = "Md five and a half"
+	}
+	if result.GenericFileHasDigest() == true {
+		t.Errorf("GenericFileHasDigest() should have returned false")
+	}
+}
+
+func TestFedoraSha256(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	if result.FedoraSha256() != sha256sum {
+		t.Errorf("FedoraSha256() should have returned", sha256sum)
+	}
+}
+
+func TestFixityCheckPossible(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	result.Sha256 = sha256sum
+	if result.FixityCheckPossible() == false {
+		t.Errorf("FixityCheckPossible() should have returned true")
+	}
+	result.Sha256 = ""
+	if result.FixityCheckPossible() == true {
+		t.Errorf("FixityCheckPossible() should have returned false")
+	}
+	result.Sha256 = sha256sum
+	// Make the SHA256 checksum disappear
+	for i := range result.GenericFile.ChecksumAttributes {
+		result.GenericFile.ChecksumAttributes[i].Algorithm = "Md five and a half"
+	}
+	if result.FixityCheckPossible() == true {
+		t.Errorf("FixityCheckPossible() should have returned false")
+	}
+}
+
+
+func TestBuildPremisEvent_Success(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	result.Sha256 = sha256sum
+	premisEvent, err := result.BuildPremisEvent()
+	if err != nil {
+		t.Errorf("BuildPremisEvent() returned an error: %v", err)
+	}
+	if len(premisEvent.Identifier) != 36 {
+		t.Errorf("PremisEvent.Identifier '%s' is not a valid UUID", premisEvent.Identifier)
+	}
+	if premisEvent.EventType != "fixity_check" {
+		t.Errorf("PremisEvent.EventType '%s' should be 'fixity_check'", premisEvent.EventType)
+	}
+	if time.Now().Unix() - premisEvent.DateTime.Unix() > 5 {
+		t.Errorf("PremisEvent.DateTime should be close to current time, but it's not.")
+	}
+	if premisEvent.Detail != "Fixity check against registered hash" {
+		t.Errorf("Unexpected PremisEvent.Detail '%s'", premisEvent.Detail)
+	}
+	if premisEvent.Outcome != "success" {
+		t.Errorf("PremisEvent.Outcome expected 'success' but got '%s'", premisEvent.Outcome)
+	}
+	if premisEvent.OutcomeDetail != sha256sum {
+		t.Errorf("PremisEvent.OutcomeDetail expected '%s' but got '%s'",
+			sha256sum, premisEvent.OutcomeDetail)
+	}
+	if premisEvent.Object != "Go language cryptohash" {
+		t.Errorf("PremisEvent.Outcome expected 'Go language cryptohash' but got '%s'",
+			premisEvent.Object)
+	}
+	if premisEvent.Agent != "http://golang.org/pkg/crypto/sha256/" {
+		t.Errorf("PremisEvent.Outcome expected 'http://golang.org/pkg/crypto/sha256/' but got '%s'",
+			premisEvent.Agent)
+	}
+	if premisEvent.OutcomeInformation != "Fixity matches" {
+		t.Errorf("PremisEvent.OutcomeInformation expected 'Fixity matches' but got '%s'",
+			premisEvent.OutcomeInformation)
+	}
+}
+
+func TestBuildPremisEvent_Failure(t *testing.T) {
+	result := result.NewFixityResult(getGenericFile())
+	result.Sha256 = "xxx-xxx-xxx"
+	premisEvent, err := result.BuildPremisEvent()
+	if err != nil {
+		t.Errorf("BuildPremisEvent() returned an error: %v", err)
+	}
+	if len(premisEvent.Identifier) != 36 {
+		t.Errorf("PremisEvent.Identifier '%s' is not a valid UUID", premisEvent.Identifier)
+	}
+	if premisEvent.EventType != "fixity_check" {
+		t.Errorf("PremisEvent.EventType '%s' should be 'fixity_check'", premisEvent.EventType)
+	}
+	if time.Now().Unix() - premisEvent.DateTime.Unix() > 5 {
+		t.Errorf("PremisEvent.DateTime should be close to current time, but it's not.")
+	}
+	if premisEvent.Detail != "Fixity does not match expected value" {
+		t.Errorf("Unexpected PremisEvent.Detail '%s'", premisEvent.Detail)
+	}
+	if premisEvent.Outcome != "failure" {
+		t.Errorf("PremisEvent.Outcome expected 'failure' but got '%s'", premisEvent.Outcome)
+	}
+	if premisEvent.OutcomeDetail != result.Sha256 {
+		t.Errorf("PremisEvent.OutcomeDetail expected '%s' but got '%s'",
+			sha256sum, premisEvent.OutcomeDetail)
+	}
+	if premisEvent.Object != "Go language cryptohash" {
+		t.Errorf("PremisEvent.Outcome expected 'Go language cryptohash' but got '%s'",
+			premisEvent.Object)
+	}
+	if premisEvent.Agent != "http://golang.org/pkg/crypto/sha256/" {
+		t.Errorf("PremisEvent.Outcome expected 'http://golang.org/pkg/crypto/sha256/' but got '%s'",
+			premisEvent.Agent)
+	}
+	if premisEvent.OutcomeInformation != "Expected digest 'fedcba9876543210', got 'xxx-xxx-xxx'" {
+		t.Errorf("PremisEvent.OutcomeInformation expected '%s' but got '%s'",
+			result.Result.Errors[0], premisEvent.OutcomeInformation)
+	}
+}
