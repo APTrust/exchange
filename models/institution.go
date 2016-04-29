5b66f16de39d08153d3f60426d822c72ec58409c
A. Diamond <diamond@sheprador.com>
2016-04-29 16:35:51 -0400

    Changed Institution pid to id (int)

--- models/institution.go
+++ models/institution.go
@@ -3,8 +3,8 @@ package models
 // Institution represents an institution in Fuctus.
 
 type Institution struct {
-	// Pid is the Fedora pid for this object.
-	Pid        string `json:"pid"`
+	// Id is the Pharos id for this object.
+	Id         int    `json:"id"`
 
 	// Name is the institution's full name.
 	Name       string `json:"name"`
