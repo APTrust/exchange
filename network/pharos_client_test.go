3076ba94d7b3fa83af66b6e0477ea3bd60b35a77
A. Diamond <diamond@sheprador.com>
2016-05-02 16:07:10 -0400

    Starting PharosClient tests

--- network/pharos_client_test.go
+++ network/pharos_client_test.go
@@ -3,16 +3,21 @@ package network_test
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/APTrust/exchange/models"
+//	"github.com/APTrust/exchange/models"
 	"github.com/APTrust/exchange/network"
+	"github.com/APTrust/exchange/testdata"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestNewPharosClient(t *testing.T) {
 
+func TestNewPharosClient(t *testing.T) {
+	_, err := network.NewPharosClient("http://example.com", "v1", "user", "key")
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestInstitutionGet(t *testing.T) {
@@ -26,23 +31,19 @@ func TestInstitutionGet(t *testing.T) {
 	}
 
 	response := client.InstitutionGet("college.edu")
-
-	respData, _ := response.RawResponseData()
-	fmt.Println(string(respData))
 	assert.Nil(t, response.Error)
+	assert.EqualValues(t, "Institution", response.ObjectType())
+	if response.Institution() == nil {
+		t.Errorf("Institution should not be nil")
+	}
+	assert.NotEqual(t, "", len(response.Institution().Identifier))
 }
 
 
 // HTTP test server handler functions
 
 func institutionGetHander(w http.ResponseWriter, r *http.Request) {
-
-	// ---------------------------------------------------
-	// TODO: Look into https://github.com/icrowley/fake
-	// for generating test data.
-	// ---------------------------------------------------
-
-	inst := models.Institution{}
+	inst := testdata.MakeInstitution()
 	instJson, _ := json.Marshal(inst)
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintln(w, string(instJson))
