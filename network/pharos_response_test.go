f81569bb2e2f9d78ec320b5b132bf734d46d59f6
A. Diamond <diamond@sheprador.com>
2016-05-05 11:16:44 -0400

    More response tests

--- network/pharos_response_test.go
+++ network/pharos_response_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/APTrust/exchange/network"
 //	"github.com/APTrust/exchange/testdata"
 	"github.com/stretchr/testify/assert"
-//	"net/http"
-//	"net/http/httptest"
+	"net/http"
+	"net/http/httptest"
 //	"net/url"
 //	"os"
 //	"strings"
@@ -36,27 +36,69 @@ func TestNewPharosResponse(t *testing.T) {
 }
 
 func TestRawResponseData(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(institutionGetHander))
+	defer testServer.Close()
+	client, err := network.NewPharosClient(testServer.URL, "v1", "user", "key")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	resp := client.InstitutionGet("college.edu")
 
+	// Should be able to call repeatedly without error.
+	// Incorrect implementation would try to read from
+	// closed network socket.
+	for i := 0; i < 3; i++ {
+		bytes, err := resp.RawResponseData()
+		assert.NotNil(t, bytes)
+		assert.NotEmpty(t, bytes)
+		assert.Nil(t, err)
+	}
 }
 
 func TestObjectType(t *testing.T) {
-
+	for _, objType := range objectTypes {
+		resp := network.NewPharosResponse(objType)
+		assert.Equal(t, objType, resp.ObjectType())
+	}
 }
 
 func TestHasNextPage(t *testing.T) {
-
+	resp := network.NewPharosResponse(network.PharosInstitution)
+	assert.False(t, resp.HasNextPage())
+	link := "http://example.com"
+	resp.Next = &link
+	assert.True(t, resp.HasNextPage())
 }
 
 func TestHasPreviousPage(t *testing.T) {
-
+	resp := network.NewPharosResponse(network.PharosInstitution)
+	assert.False(t, resp.HasPreviousPage())
+	link := "http://example.com"
+	resp.Previous = &link
+	assert.True(t, resp.HasPreviousPage())
 }
 
 func TestParamsForNextPage(t *testing.T) {
-
+	resp := network.NewPharosResponse(network.PharosInstitution)
+	link := "http://example.com?name=college.edu&page=6&per_page=20"
+	resp.Next = &link
+	params := resp.ParamsForNextPage()
+	assert.Equal(t, 3, len(params))
+	assert.Equal(t, "college.edu", params.Get("name"))
+	assert.Equal(t, "6", params.Get("page"))
+	assert.Equal(t, "20", params.Get("per_page"))
 }
 
 func TestParamsForPreviousPage(t *testing.T) {
-
+	resp := network.NewPharosResponse(network.PharosInstitution)
+	link := "http://example.com?name=college.edu&page=6&per_page=20"
+	resp.Previous = &link
+	params := resp.ParamsForPreviousPage()
+	assert.Equal(t, 3, len(params))
+	assert.Equal(t, "college.edu", params.Get("name"))
+	assert.Equal(t, "6", params.Get("page"))
+	assert.Equal(t, "20", params.Get("per_page"))
 }
 
 func TestInstitution(t *testing.T) {
