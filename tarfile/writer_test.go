4455afad01b81a33761892a545e37fbc731c7f83
A. Diamond <diamond@sheprador.com>
2016-05-19 10:56:06 -0400

    Working on tests for tar writer

--- tarfile/writer_test.go
+++ tarfile/writer_test.go
@@ -2,12 +2,43 @@ package tarfile_test
 
 import (
 //	"fmt"
-//	"github.com/APTrust/exchange/tarfile"
-//	"github.com/stretchr/testify/assert"
+	"github.com/APTrust/exchange/tarfile"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestNewWriter(t *testing.T) {
+	dir, err := ioutil.TempDir("", "tarwriter_test")
+	if err != nil {
+		assert.FailNow(t, "Cannot create temp dir", err.Error())
+	}
+	tempFilePath := filepath.Join(dir, "test_file.tar")
+	defer os.RemoveAll(dir)
+	w := tarfile.NewWriter(tempFilePath)
+	assert.NotNil(t, w)
+	assert.Equal(t, tempFilePath, w.PathToTarFile)
+}
+
+func TestOpen(t *testing.T) {
+	dir, err := ioutil.TempDir("", "tarwriter_test")
+	if err != nil {
+		assert.FailNow(t, "Cannot create temp dir", err.Error())
+	}
+	tempFilePath := filepath.Join(dir, "test_file.tar")
+	defer os.RemoveAll(dir)
+	w := tarfile.NewWriter(tempFilePath)
+	defer w.Close()
+	err = w.Open()
+	assert.Nil(t, err)
+	if _, err := os.Stat(w.PathToTarFile); os.IsNotExist(err) {
+		assert.Fail(t, "Tar file does not exist at %s", w.PathToTarFile)
+	}
+}
+
+func TestClose(t *testing.T) {
 
 }
 
