9051c164b41d2d9dbfe1ae446f9690482edb40a9
A. Diamond <diamond@sheprador.com>
2016-05-25 16:38:44 -0400

    Finished tests for TarFileIterator

--- util/fileutil/tar_file_iterator_test.go
+++ util/fileutil/tar_file_iterator_test.go
@@ -61,14 +61,28 @@ func TestNext(t *testing.T) {
 	}
 }
 
+// Should be able to close repeatedly without panic.
 func TestTarFileIteratorClose(t *testing.T) {
-
+	_, filename, _, _ := runtime.Caller(0)
+	tarFilePath, _ := filepath.Abs(path.Join(filepath.Dir(filename),
+		"..", "..", "testdata", "example.edu.tagsample_good.tar"))
+	tfi, _ := fileutil.NewTarFileIterator(tarFilePath)
+	if tfi == nil {
+		assert.Fail(t, "Could not get TarFileIterator")
+	}
+	assert.NotPanics(t, tfi.Close, "TarFileIterator.Close() freaked out")
+	assert.NotPanics(t, tfi.Close, "TarFileIterator.Close() freaked out")
 }
 
 func TestRead(t *testing.T) {
-
+	// This is tested above, in the call to reader.Read(buf)
 }
 
+// Should be able to close repeatedly without error.
 func TestTarReaderCloserClose(t *testing.T) {
-
+	trc := fileutil.TarReadCloser{}
+	err := trc.Close()
+	assert.Nil(t, err)
+	err = trc.Close()
+	assert.Nil(t, err)
 }
