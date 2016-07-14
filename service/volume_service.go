5bc76a4c23dbe291391ffcf502a3f00b8235bfdf
A. Diamond <diamond@sheprador.com>
2016-07-14 17:03:14 -0400

    Added handler stubs.

--- service/volume_service.go
+++ service/volume_service.go
@@ -1,13 +1,15 @@
 // +build !partners
 
 // Don't include this in the partners build: it's not needed
-// in the partner apps, and the syscall.Stat* functions cause
-// the build to fail on Windows.
+// in the partner apps, and the syscall.Stat* functions inside
+// the models.Volume code cause the build to fail on Windows.
 package service
 
 import (
+	"fmt"
 	"github.com/APTrust/exchange/models"
 	"github.com/op/go-logging"
+	"net/http"
 )
 
 type VolumeService struct {
@@ -25,4 +27,26 @@ func NewVolumeService(port int, messageLog *logging.Logger) (*VolumeService) {
 		volumes: make(map[string]*models.Volume),
 		messageLog: messageLog,
 	}
+}
+
+// Returns a Volume object with info about the volume at the specified
+// mount point. The mount point should be the path to a disk or partition.
+// For example, "/", "/mnt/data", etc.
+func (service *VolumeService) getVolume(mountPoint string) (*models.Volume) {
+	if _, keyExists := service.volumes[mountPoint]; !keyExists {
+		service.volumes[mountPoint] = models.NewVolume(mountPoint)
+	}
+	return service.volumes[mountPoint]
+}
+
+func reserveHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "")
+}
+
+func releaseHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func reportHandler(w http.ResponseWriter, r *http.Request) {
+
 }
