31d489f328cbe45cbf73025898bcc5e4a8b816c1
A. Diamond <diamond@sheprador.com>
2016-09-08 11:55:51 -0400

    Working on fetcher

--- models/work_item_state.go
+++ models/work_item_state.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/APTrust/exchange/constants"
 	"time"
 )
 
@@ -19,4 +22,39 @@ func NewWorkItemState(workItemId int, action, state string) (*WorkItemState) {
 		Action: action,
 		State: state,
 	}
+}
+
+func (state *WorkItemState) HasData() (bool) {
+	return state.State != ""
+}
+
+// IngestManifest converts the State string (JSON) to an IngestManifest
+// object. This works only if there's data in the State string, and the
+// Action is constants.ActionIngest. Other actions will have different
+// types of data in the State string.
+func (state *WorkItemState) IngestManifest() (*IngestManifest, error) {
+	if !state.HasData() {
+		return nil, fmt.Errorf("Cannot convert state to IngestManifest because state is empty.")
+	}
+	if state.Action != constants.ActionIngest {
+		return nil, fmt.Errorf("Cannot convert state to IngestManifest because action is '%s' " +
+			"and must be '%s'.", state.Action, constants.ActionIngest)
+	}
+	var ingestManifest *IngestManifest
+	err := json.Unmarshal([]byte(state.State), ingestManifest)
+	return ingestManifest, err
+}
+
+// Converts an IngestManifest into a JSON string and stores it in the State
+// attribute.
+func (state *WorkItemState) SetStateFromIngestManifest(manifest *IngestManifest) (error) {
+	if state.Action != constants.ActionIngest {
+		return fmt.Errorf("Cannot set state from IngestManifest because action is '%s' " +
+			"and must be '%s'.", state.Action, constants.ActionIngest)
+	}
+	jsonData, err := json.MarshalIndent(manifest, "", "  ")
+	if err != nil {
+		state.State = string(jsonData)
+	}
+	return err
 }
--- workers/apt_fetcher.go
+++ workers/apt_fetcher.go
@@ -1,10 +1,13 @@
 package workers
 
 import (
-//	"github.com/APTrust/exchange/constants"
+	"fmt"
+	"github.com/APTrust/exchange/constants"
 	"github.com/APTrust/exchange/context"
 	"github.com/APTrust/exchange/models"
 	"github.com/nsqio/go-nsq"
+	"net/http"
+	"strconv"
 	"sync"
 //	"time"
 )
@@ -42,6 +45,23 @@ func NewATPFetcher(_context *context.Context) (*APTFetcher) {
 }
 
 func (fetcher *APTFetcher) HandleMessage(message *nsq.Message) (error) {
+	message.DisableAutoResponse()
+	workItem, err := fetcher.getWorkItem(message)
+	if err != nil {
+		fetcher.Context.MessageLog.Error(err.Error())
+		return err
+	}
+	workItemState, err := fetcher.getWorkItemState(workItem)
+	if err != nil {
+		fetcher.Context.MessageLog.Error(err.Error())
+		return err
+	}
+	ingestManifest, err := workItemState.IngestManifest()
+	if err != nil {
+		fetcher.Context.MessageLog.Error(err.Error())
+		return err
+	}
+	fetcher.FetchChannel <- ingestManifest
 	return nil
 }
 
@@ -69,6 +89,67 @@ func (fetcher *APTFetcher) record() {
 	// }
 }
 
+// Returns the WorkItem record from Pharos that has the WorkItemId
+// specified in the NSQ message.
+func (fetcher *APTFetcher) getWorkItem(message *nsq.Message) (*models.WorkItem, error) {
+	workItemId, err := strconv.Atoi(string(message.Body))
+	if err != nil {
+		return nil, fmt.Errorf("Could not get WorkItemId from NSQ message body: %v", err)
+	}
+	resp := fetcher.Context.PharosClient.WorkItemGet(workItemId)
+	if resp.Error != nil {
+		return nil, fmt.Errorf("Error getting WorkItem %d from Pharos: %v", err)
+	}
+	workItem := resp.WorkItem()
+	if workItem == nil {
+		return nil, fmt.Errorf("Pharos returned nil for WorkItem %d", workItemId)
+	}
+	return workItem, nil
+}
+
+// Returns the WorkItemState record from Pharos with the specified workItem.Id,
+// or creates a new WorkItemState (if necessary) and returns that. If this is
+// the first time we've attempted to ingest this item, we'll have to crate a
+// new WorkItemState.
+func (fetcher *APTFetcher) getWorkItemState(workItem *models.WorkItem) (*models.WorkItemState, error) {
+	var workItemState *models.WorkItemState
+	var err error
+	resp := fetcher.Context.PharosClient.WorkItemStateGet(workItem.Id)
+	if resp.Response.StatusCode == http.StatusNotFound {
+		// Record has not been created yet, so build a new one now.
+		workItemState, err = fetcher.InitWorkItemState(workItem)
+		if err != nil {
+			return nil, err
+		}
+	} else if resp.Error != nil {
+		// We got some other 4xx/5xx error from the Pharos REST service.
+		return nil, fmt.Errorf("Error getting WorkItemState for WorkItem %d from Pharos: %v", resp.Error)
+	} else {
+		// We didn't get a 404 or any other error. The WorkItemState should be in
+		// the response.
+		workItemState = resp.WorkItemState()
+		if workItemState == nil {
+			return nil, fmt.Errorf("Pharos returned nil for WorkItemState with WorkItem id %d", workItem.Id)
+		}
+	}
+	return workItemState, nil
+}
+
+func (fetcher *APTFetcher) InitWorkItemState (workItem *models.WorkItem) (*models.WorkItemState, error) {
+	ingestManifest := models.NewIngestManifest()
+	ingestManifest.WorkItemId = workItem.Id
+	ingestManifest.S3Bucket = workItem.Bucket
+	ingestManifest.S3Key = workItem.Name
+	ingestManifest.ETag = workItem.ETag
+	workItemState := models.NewWorkItemState(workItem.Id, constants.ActionIngest, "")
+	err := workItemState.SetStateFromIngestManifest(ingestManifest)
+	if err != nil {
+		return nil, err
+	}
+	return workItemState, nil
+}
+
+// This is for direct testing without NSQ.
 func (fetcher *APTFetcher) RunWithoutNsq(manifest *models.IngestManifest) {
 	fetcher.WaitGroup.Add(1)
 	fetcher.InitChannel <- manifest
