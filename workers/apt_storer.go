eca3ace9c6d009dd4ab03884daa9eba30d619d45
A. Diamond <diamond@sheprador.com>
2016-09-14 13:54:18 -0400

    Stubbing apt_storer.go

--- workers/apt_storer.go
+++ workers/apt_storer.go
@@ -2,7 +2,7 @@ package workers
 
 import (
 //	"fmt"
-//	"github.com/APTrust/exchange/constants"
+	"github.com/APTrust/exchange/constants"
 	"github.com/APTrust/exchange/context"
 	"github.com/APTrust/exchange/models"
 //	"github.com/APTrust/exchange/network"
@@ -39,7 +39,7 @@ func NewAPTStorer(_context *context.Context) (*APTStorer) {
 	storer.CleanupChannel = make(chan *models.IngestState, workerBufferSize)
 	storer.RecordChannel = make(chan *models.IngestState, workerBufferSize)
 	// Set up a limited number of go routines
-	for i := 0; i < _context.Config.FetchWorker.Workers; i++ {
+	for i := 0; i < _context.Config.StoreWorker.Workers; i++ {
 		go storer.store()
 		go storer.cleanup()
 		go storer.record()
@@ -84,34 +84,102 @@ func (storer *APTStorer) HandleMessage(message *nsq.Message) (error) {
 	return nil
 }
 
+// -------------------------------------------------------------------------
+// Step 1 of 3: Put the item in long-term storage
+//
+// -------------------------------------------------------------------------
 func (storer *APTStorer) store () {
-
+	for ingestState := range storer.StorageChannel {
+		existingObj, err := storer.getExistingObject(ingestState.IngestManifest.Object.Identifier)
+		if err != nil {
+			ingestState.IngestManifest.StoreResult.AddError(err.Error())
+		} else {
+			for _, gf := range ingestState.IngestManifest.Object.GenericFiles {
+				if existingObj != nil {
+					existingFile := existingObj.FindGenericFile(gf.OriginalPath())
+					if existingFile != nil {
+						existingSha256 := existingFile.GetChecksum(constants.AlgSha256)
+						if existingSha256.Digest == gf.IngestSha256 {
+							gf.IngestNeedsSave = false
+						}
+					}
+				}
+				if gf.IngestNeedsSave {
+					if gf.IngestStoredAt.IsZero() {
+						storer.copyToPrimaryStorage(ingestState, gf)
+					}
+					if gf.IngestReplicatedAt.IsZero() {
+						storer.copyToSecondaryStorage(ingestState, gf)
+					}
+				}
+			}
+		}
+		storer.CleanupChannel <- ingestState
+	}
 }
 
+// -------------------------------------------------------------------------
+// Step 2 of 3: Delete the bag file(s) if storage succeeded
+//
+// -------------------------------------------------------------------------
 func (storer *APTStorer) cleanup () {
+//	for ingestState := range storer.CleanupChannel {
 
+//	}
 }
 
+// -------------------------------------------------------------------------
+// Step 3 of 3: Record IntellectualObject and GenericFile data in Pharos
+//
+// -------------------------------------------------------------------------
 func (storer *APTStorer) record () {
+//	for ingestState := range storer.RecordChannel {
 
+//	}
 }
 
 func (storer *APTStorer) loadIngestState (message *nsq.Message) (*models.IngestState, error) {
+	// Load WorkItem and WorkItemState
+	// Get IngestManifest from WorkItemState
 	return nil, nil
 }
 
+func (storer *APTStorer) getExistingObject (objectIdentifier string) (*models.IntellectualObject, error) {
+	// Get the IntellectualObject from Pharos
+	// If it exists, get the GenericFiles and add them in
+	return nil, nil
+}
+
+// Copy the GenericFile to primary storage (S3)
+func (storer *APTStorer) copyToPrimaryStorage (ingestState *models.IngestState, gf *models.GenericFile) {
+
+}
+
+// Copy the GenericFile to secondary storage (Glacier)
+func (storer *APTStorer) copyToSecondaryStorage (ingestState *models.IngestState, gf *models.GenericFile) {
+
+}
+
+// Tell Pharos we've started copying this item's files to
+// long-term storage.
 func (storer *APTStorer) markWorkItemStarted (ingestState *models.IngestState) (*models.WorkItem, error) {
 	return nil, nil
 }
 
+// Tell Pharos we finished copying all files to long-term
+// storage (successfully).
 func (storer *APTStorer) markWorkItemSucceeded (ingestState *models.IngestState) (*models.WorkItem, error) {
 	return nil, nil
 }
 
+// Tell Pharos that the storage attempt failed with a
+// fatal error, and should not be retried.
 func (storer *APTStorer) markWorkItemFailed (ingestState *models.IngestState) (*models.WorkItem, error) {
 	return nil, nil
 }
 
+// Tell Pharos that storage failed with a transient
+// error, and we should try this item again later.
 func (storer *APTStorer) markWorkItemRequeued (ingestState *models.IngestState) (*models.WorkItem, error) {
 	return nil, nil
 }
