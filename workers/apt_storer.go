7e0a1714ea6de35ae1ce3ff23eb617ca3fbea813
A. Diamond <diamond@sheprador.com>
2016-09-16 15:16:33 -0400

    apt_storer now uses some common worker methods

--- workers/apt_storer.go
+++ workers/apt_storer.go
@@ -70,7 +70,8 @@ func (storer *APTStorer) HandleMessage(message *nsq.Message) (error) {
 	ingestState.IngestManifest.StoreResult.ClearErrors()
 
 	// Tell Pharos that we've started to store this item.
-	ingestState.WorkItem, err = storer.markWorkItemStarted(ingestState)
+	err = MarkWorkItemStarted(ingestState, storer.Context,
+		constants.StageStore, "Files are being copied to long-term storage.")
 	if err != nil {
 		storer.Context.MessageLog.Error(err.Error())
 		return err
@@ -141,15 +142,14 @@ func (storer *APTStorer) record () {
 
 		if ingestState.IngestManifest.HasFatalErrors() {
 			ingestState.FinishNSQ()
-			storer.markWorkItemFailed(ingestState)
+			MarkWorkItemFailed(ingestState, storer.Context)
 		} else if ingestState.IngestManifest.HasErrors() {
 			ingestState.RequeueNSQ(1000)
-			storer.markWorkItemRequeued(ingestState)
+			MarkWorkItemRequeued(ingestState, storer.Context)
 		} else {
 			ingestState.FinishNSQ()
-			storer.markWorkItemSucceeded(ingestState)
-			// Move to next queue
-			// If item can't be queued, RecordWorkItemState again
+			MarkWorkItemSucceeded(ingestState, storer.Context, constants.StageRecord)
+			PushToQueue(ingestState, storer.Context, storer.Context.Config.RecordWorker.NsqTopic)
 		}
 	}
 }
@@ -328,88 +328,4 @@ func (storer *APTStorer) markFileAsStored (gf *models.GenericFile, sendWhere, st
 		gf.IngestReplicatedAt = time.Now().UTC()
 		gf.IngestReplicationURL = storageUrl
 	}
-}
-// Tell Pharos we've started copying this item's files to
-// long-term storage.
-func (storer *APTStorer) markWorkItemStarted (ingestState *models.IngestState) (*models.WorkItem, error) {
-	storer.Context.MessageLog.Info("Telling Pharos record started for %s/%s",
-		ingestState.WorkItem.Bucket, ingestState.WorkItem.Name)
-	utcNow := time.Now().UTC()
-	ingestState.WorkItem.SetNodeAndPid()
-	ingestState.WorkItem.Stage = constants.StageFetch
-	ingestState.WorkItem.StageStartedAt = &utcNow
-	ingestState.WorkItem.Status = constants.StatusStarted
-	ingestState.WorkItem.Note = "Copying files to long-term storage"
-	resp := storer.Context.PharosClient.WorkItemSave(ingestState.WorkItem)
-	if resp.Error != nil {
-		return nil, resp.Error
-	}
-	return resp.WorkItem(), nil
-}
-
-// Tell Pharos we finished copying all files to long-term
-// storage (successfully).
-func (storer *APTStorer) markWorkItemSucceeded (ingestState *models.IngestState) (error) {
-	storer.Context.MessageLog.Info("Telling Pharos storage succeeded for %s/%s",
-		ingestState.WorkItem.Bucket, ingestState.WorkItem.Name)
-	ingestState.WorkItem.Node = ""
-	ingestState.WorkItem.Pid = 0
-	ingestState.WorkItem.Retry = true
-	ingestState.WorkItem.StageStartedAt = nil
-	ingestState.WorkItem.NeedsAdminReview = false
-	ingestState.WorkItem.Stage = constants.StageRecord
-	ingestState.WorkItem.Status = constants.StatusPending
-	ingestState.WorkItem.Note = "All files copied to S3 and Glacier. Awaiting metadata recording in Pharos."
-	resp := storer.Context.PharosClient.WorkItemSave(ingestState.WorkItem)
-	if resp.Error != nil {
-		storer.Context.MessageLog.Error("Could not mark WorkItem ready for record for %s/%s: %v",
-			ingestState.WorkItem.Bucket, ingestState.WorkItem.Name, resp.Error)
-		return resp.Error
-	}
-	ingestState.WorkItem = resp.WorkItem()
-	return nil
-}
-
-// Tell Pharos that the storage attempt failed with a
-// fatal error, and should not be retried.
-func (storer *APTStorer) markWorkItemFailed (ingestState *models.IngestState) (error) {
-	storer.Context.MessageLog.Info("Telling Pharos storage failed for %s/%s",
-		ingestState.WorkItem.Bucket, ingestState.WorkItem.Name)
-	ingestState.WorkItem.Node = ""
-	ingestState.WorkItem.Pid = 0
-	ingestState.WorkItem.StageStartedAt = nil
-	ingestState.WorkItem.Retry = false
-	ingestState.WorkItem.NeedsAdminReview = true
-	ingestState.WorkItem.Status = constants.StatusFailed
-	ingestState.WorkItem.Note = ingestState.IngestManifest.FetchResult.AllErrorsAsString() + ingestState.IngestManifest.ValidateResult.AllErrorsAsString()
-	resp := storer.Context.PharosClient.WorkItemSave(ingestState.WorkItem)
-	if resp.Error != nil {
-		storer.Context.MessageLog.Error("Could not mark WorkItem failed for %s/%s: %v",
-			ingestState.WorkItem.Bucket, ingestState.WorkItem.Name, resp.Error)
-		return resp.Error
-	}
-	ingestState.WorkItem = resp.WorkItem()
-	return nil
-}
-
-// Tell Pharos that storage failed with a transient
-// error, and we should try this item again later.
-func (storer *APTStorer) markWorkItemRequeued (ingestState *models.IngestState) (error) {
-	storer.Context.MessageLog.Info("Telling Pharos storage is being requeued for %s/%s",
-		ingestState.WorkItem.Bucket, ingestState.WorkItem.Name)
-	ingestState.WorkItem.Node = ""
-	ingestState.WorkItem.Pid = 0
-	ingestState.WorkItem.StageStartedAt = nil
-	ingestState.WorkItem.Retry = true
-	ingestState.WorkItem.NeedsAdminReview = false
-	ingestState.WorkItem.Status = constants.StatusStarted
-	ingestState.WorkItem.Note = "Item has been requeued due to transient errors. " + ingestState.IngestManifest.FetchResult.AllErrorsAsString() + ingestState.IngestManifest.ValidateResult.AllErrorsAsString()
-	resp := storer.Context.PharosClient.WorkItemSave(ingestState.WorkItem)
-	if resp.Error != nil {
-		storer.Context.MessageLog.Error("Could not mark WorkItem requeued for %s/%s: %v",
-			ingestState.WorkItem.Bucket, ingestState.WorkItem.Name, resp.Error)
-		return resp.Error
-	}
-	ingestState.WorkItem = resp.WorkItem()
-	return nil
 }
