e0f2ad7a0f493b014afde1a111349adcbde04283
A. Diamond <diamond@sheprador.com>
2016-10-08 13:44:59 -0400

    Added specific spot tests for one object and file

--- integration/apt_fetch_test.go
+++ integration/apt_fetch_test.go
@@ -2,11 +2,13 @@ package integration_test
 
 import (
 	"github.com/APTrust/exchange/models"
+	"github.com/APTrust/exchange/util"
 	"github.com/APTrust/exchange/util/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 /*
@@ -38,6 +40,9 @@ func TestFetchResults(t *testing.T) {
 		}
 		fetcherTestCommon(t, bagName, ingestManifest)
 		fetcherTestGoodBagResult(t, bagName, ingestManifest)
+		if bagName == "aptrust.receiving.test.test.edu/example.edu.tagsample_good.tar" {
+			fetcherTestSpecifics(t, ingestManifest)
+		}
 	}
 	for _, bagName := range testutil.INTEGRATION_BAD_BAGS {
 		ingestManifest, err := testutil.FindResultInLog(pathToJsonLog, bagName)
@@ -148,4 +153,60 @@ func fetcherTestCommon(t *testing.T, bagName string, ingestManifest *models.Inge
 		"Object.IngestS3Key should not be empty for %s", bagName)
 	assert.NotEmpty(t, ingestManifest.Object.IngestTarFilePath,
 		"Object.IngestTarFilePath should not be empty for %s", bagName)
+}
+
+func fetcherTestSpecifics(t *testing.T, ingestManifest *models.IngestManifest) {
+	obj := ingestManifest.Object
+	assert.Equal(t, "test.edu/example.edu.tagsample_good", obj.Identifier)
+	assert.Equal(t, "example.edu.tagsample_good", obj.BagName)
+	assert.Equal(t, "test.edu", obj.Institution)
+	assert.NotEqual(t, 0, obj.InstitutionId)
+	assert.Equal(t, "Tag Sample (Good)", obj.Title)
+	assert.Equal(t, "Bag of goodies", obj.Description)
+	assert.Equal(t, "Institution", obj.Access)
+	assert.Equal(t, "uva-internal-id-0001", obj.AltIdentifier)
+	assert.Equal(t, "aptrust.receiving.test.test.edu", obj.IngestS3Bucket)
+	assert.Equal(t, "example.edu.tagsample_good.tar", obj.IngestS3Key)
+	assert.Equal(t, "manifest-md5.txt", obj.IngestManifests[0])
+	assert.Equal(t, "manifest-sha256.txt", obj.IngestManifests[1])
+	assert.Equal(t, "tagmanifest-md5.txt", obj.IngestTagManifests[0])
+	assert.Equal(t, "tagmanifest-sha256.txt", obj.IngestTagManifests[1])
+	assert.Empty(t, obj.IngestFilesIgnored)
+	assert.Equal(t, "example.edu.tagsample_good", obj.IngestTopLevelDirNames[0])
+	assert.Empty(t, obj.IngestErrorMessage)
+
+	assert.Equal(t, 10, len(obj.IngestTags))
+	assert.Equal(t, "bag-info.txt", obj.IngestTags[5].SourceFile)
+	assert.Equal(t, "Bag-Group-Identifier", obj.IngestTags[5].Label)
+	assert.Equal(t, "Charley Horse", obj.IngestTags[5].Value)
+
+	assert.Empty(t, obj.PremisEvents)
+
+	assert.Equal(t, 16, len(obj.GenericFiles))
+
+	gf := obj.GenericFiles[0]
+	assert.Equal(t, 0, gf.Id)
+	assert.Equal(t, "test.edu/example.edu.tagsample_good/aptrust-info.txt", gf.Identifier)
+	assert.Equal(t, 0, gf.IntellectualObjectId)
+	assert.Equal(t, "test.edu/example.edu.tagsample_good", gf.IntellectualObjectIdentifier)
+	assert.Equal(t, "text/plain", gf.FileFormat)
+	assert.EqualValues(t, 45, gf.Size)
+	assert.EqualValues(t, "0001-01-01T00:00:00Z", gf.FileCreated.Format(time.RFC3339))
+	assert.Equal(t, "2016-03-21T11:01:51-04:00", gf.FileModified.Format(time.RFC3339))
+	assert.Empty(t, gf.Checksums)
+	assert.Empty(t, gf.PremisEvents)
+	assert.Equal(t, "tag_file", gf.IngestFileType)
+	assert.Equal(t, "bd8be664c790a9175e9d2fe90b40d502", gf.IngestMd5)
+	assert.False(t, gf.IngestMd5GeneratedAt.IsZero())
+	assert.False(t, gf.IngestMd5VerifiedAt.IsZero())
+	assert.Equal(t, "49dd23cdaf644e60629f01d6ebea770cd1c6229ff89f14a6c030a50c48b6ba27", gf.IngestSha256)
+	assert.False(t, gf.IngestSha256GeneratedAt.IsZero())
+	assert.False(t, gf.IngestSha256VerifiedAt.IsZero())
+	assert.True(t, util.LooksLikeUUID(gf.IngestUUID))
+	assert.False(t, gf.IngestUUIDGeneratedAt.IsZero())
+	assert.True(t, gf.IngestStoredAt.IsZero())
+	assert.True(t, gf.IngestReplicatedAt.IsZero())
+	assert.True(t, gf.IngestNeedsSave)
+	assert.EqualValues(t, 502, gf.IngestFileUid)
+	assert.EqualValues(t, 20, gf.IngestFileGid)
 }
