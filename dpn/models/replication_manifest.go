387559f032e112f7e71916dd9d30f228e0a2aa03
A. Diamond <diamond@sheprador.com>
2016-10-31 11:35:23 -0400

    Added ReplicationManifest

---
+++ dpn/models/replication_manifest.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	apt_models "github.com/APTrust/exchange/models"
+	"github.com/nsqio/go-nsq"
+)
+
+// ReplicationManifest contains information about the processing
+// of a DPN ReplicationTransfer request.
+type ReplicationManifest struct {
+	NsqMessage          *nsq.Message `json:"-"`
+	DPNWorkItem         *apt_models.DPNWorkItem
+	ReplicationTransfer *ReplicationTransfer
+	DPNBag              *DPNBag
+	CopySummary         *apt_models.WorkSummary
+	ValidateSummary     *apt_models.WorkSummary
+	StoreSummary        *apt_models.WorkSummary
+	LocalPath           string
+	RsyncStdout         string
+	RsyncStderr         string
+}
+
+func NewReplicationManifest(nsqMessage *nsq.Message) (*ReplicationManifest) {
+	return &ReplicationManifest{
+		NsqMessage: nsqMessage,
+		CopySummary: apt_models.NewWorkSummary(),
+		ValidateSummary: apt_models.NewWorkSummary(),
+		StoreSummary: apt_models.NewWorkSummary(),
+	}
+}
