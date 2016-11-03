08d67a156797e1eaca718ebda9809d323b9e1a0c
A. Diamond <diamond@sheprador.com>
2016-11-03 16:28:34 -0400

    Refactoring service.rb

--- scripts/service.rb
+++ scripts/service.rb
@@ -9,10 +9,14 @@ class Service
   attr_reader :dpn_server_root, :exchange_root, :pharos_root
 
   def initialize
+    @apt_fetch_pid = 0
+    @apt_store_pid = 0
+    @apt_record_pid = 0
+    @apt_volume_service_pid = 0
+    @dpn_cluster_pid = 0
+    @dpn_copy_pid = 0
     @nsq_pid = 0
     @pharos_pid = 0
-    @volume_service_pid = 0
-    @dpn_cluster_pid = 0
 
     @dpn_server_root = ENV['DPN_SERVER_ROOT'] || abort("Set env var DPN_SERVER_ROOT")
     @exchange_root = ENV['EXCHANGE_ROOT'] || abort("Set env var EXCHANGE_ROOT")
@@ -32,91 +36,59 @@ class Service
     env
   end
 
-  def reset_pharos_db
-    puts "Resetting Pharos DB"
-    env = env_hash
-    cmd = 'rbenv exec rake pharos:empty_db'
-    log_file = "#{@log_dir}/pharos.log"
-    pid = Process.spawn(env, cmd, chdir: @pharos_root)
-    Process.wait pid
-    puts "Finished resetting Pharos DB"
-  end
-
-  def load_pharos_fixtures
-    puts "Loading Pharos fixtures"
-    env = env_hash
-    cmd = 'rbenv exec rake db:fixtures:load'
-    log_file = "#{@log_dir}/pharos.log"
-    pid = Process.spawn(env, cmd, chdir: @pharos_root)
-    Process.wait pid
-    puts "Finished loading Pharos fixtures"
-  end
-
-  def start_pharos
-    if @pharos_pid == 0
-      env = env_hash
-      cmd = 'rbenv exec rails server'
-      log_file = "#{@log_dir}/pharos.log"
-      @pharos_pid = Process.spawn(env,
-                                  cmd,
-                                  chdir: @pharos_root,
-                                  out: [log_file, 'w'],
-                                  err: [log_file, 'w'])
-      Process.detach @pharos_pid
-      puts "Started Pharos with command '#{cmd}' and pid #{@pharos_pid}"
+  def apt_fetch_start
+    if @apt_fetch_pid == 0
+      @apt_fetch_pid = start_go_service('apt_fetch')
     end
   end
 
-  def stop_pharos
-    if @pharos_pid != 0
-      puts "Stopping Pharos (pid #{@pharos_pid})"
-      Process.kill('TERM', @pharos_pid)
-      @pharos_pid = 0
+  def apt_fetch_stop
+    if @apt_fetch_pid != 0
+      stop_go_service('apt_fetch', @apt_fetch_pid)
+      @apt_fetch_pid = 0
     end
   end
 
-  def start_nsq
-    if @nsq_pid == 0
-      env = env_hash
-      cmd = "./nsq_service -config #{@exchange_root}/config/nsq/integration.config"
-      log_file = '/dev/null'
-      @nsq_pid = Process.spawn(env,
-                                  cmd,
-                                  chdir: @go_bin_dir,
-                                  out: [log_file, 'w'],
-                                  err: [log_file, 'w'])
-      Process.detach @nsq_pid
-      puts "Started NSQ service with command '#{cmd}' and pid #{@nsq_pid}"
+  def apt_record_start
+    if @apt_record_pid == 0
+      @apt_record_pid = start_go_service('apt_record')
     end
   end
 
-  def stop_nsq
-    if @nsq_pid != 0
-      puts "Stopping NSQ service (pid #{@nsq_pid})"
-      Process.kill('TERM', @nsq_pid)
-      @nsq_pid = 0
+  def apt_record_stop
+    if @apt_record_pid != 0
+      stop_go_service('apt_record', @apt_record_pid)
+      @apt_record_pid = 0
     end
   end
 
-  def start_volume_service
-    if @volume_service_pid == 0
-      env = env_hash
-      cmd = "./apt_volume_service -config #{@exchange_root}/config/integration.json"
-      @volume_service_pid = Process.spawn(env, cmd, chdir: @go_bin_dir)
-      Process.detach @volume_service_pid
-      puts "Started volume service with command '#{cmd}' and pid #{@volume_service_pid}"
+  def apt_store_start
+    if @apt_store_pid == 0
+      @apt_store_pid = start_go_service('apt_store')
     end
   end
 
-  def stop_volume_service
-    if @volume_service_pid != 0
-      puts "Stopping volume service service (pid #{@volume_service_pid})"
-      Process.kill('TERM', @volume_service_pid)
-      @volume_service_pid = 0
+  def apt_store_stop
+    if @apt_store_pid != 0
+      stop_go_service('apt_store', @apt_store_pid)
+      @apt_store_pid = 0
     end
   end
 
-  def init_dpn_cluster
+  def apt_volume_service_start
+    if @apt_volume_service_pid == 0
+      @apt_volume_service_pid = start_go_service('apt_volume_service')
+    end
+  end
+
+  def apt_volume_service_stop
+    if @apt_volume_service_pid != 0
+      stop_go_service('apt_volume_service', @apt_volume_service_pid)
+      @apt_volume_service_pid = 0
+    end
+  end
+
+  def dpn_cluster_init
     if @dpn_cluster_pid == 0
       env = env_hash
       puts "Setting up DPN cluster"
@@ -140,7 +112,7 @@ class Service
     end
   end
 
-  def start_dpn_cluster
+  def dpn_cluster_start
     if @dpn_cluster_pid == 0
       self.init_dpn_cluster
       puts "Deleting old DPN cluster log files"
@@ -158,7 +130,7 @@ class Service
     end
   end
 
-  def stop_dpn_cluster
+  def dpn_cluster_stop
     if @dpn_cluster_pid != 0
       puts "Stopping DPN cluster (pid #{@dpn_cluster_pid})"
       Process.kill('TERM', @dpn_cluster_pid)
@@ -166,15 +138,114 @@ class Service
     end
   end
 
+  def dpn_copy_start
+    if @dpn_copy_pid == 0
+      @dpn_copy_pid = start_go_service('dpn_copy')
+    end
+  end
+
+  def dpn_copy_stop
+    if @dpn_copy_pid != 0
+      stop_go_service('dpn_copy', @dpn_copy)
+      @dpn_copy = 0
+    end
+  end
+
+  def nsq_start
+    if @nsq_pid == 0
+      env = env_hash
+      cmd = "./nsq_service -config #{@exchange_root}/config/nsq/integration.config"
+      log_file = '/dev/null'
+      @nsq_pid = Process.spawn(env,
+                                  cmd,
+                                  chdir: @go_bin_dir,
+                                  out: [log_file, 'w'],
+                                  err: [log_file, 'w'])
+      Process.detach @nsq_pid
+      puts "Started NSQ service with command '#{cmd}' and pid #{@nsq_pid}"
+    end
+  end
+
+  def nsq_stop
+    if @nsq_pid != 0
+      puts "Stopping NSQ service (pid #{@nsq_pid})"
+      Process.kill('TERM', @nsq_pid)
+      @nsq_pid = 0
+    end
+  end
+
+  def pharos_reset_db
+    puts "Resetting Pharos DB"
+    env = env_hash
+    cmd = 'rbenv exec rake pharos:empty_db'
+    log_file = "#{@log_dir}/pharos.log"
+    pid = Process.spawn(env, cmd, chdir: @pharos_root)
+    Process.wait pid
+    puts "Finished resetting Pharos DB"
+  end
+
+  def pharos_load_fixtures
+    puts "Loading Pharos fixtures"
+    env = env_hash
+    cmd = 'rbenv exec rake db:fixtures:load'
+    log_file = "#{@log_dir}/pharos.log"
+    pid = Process.spawn(env, cmd, chdir: @pharos_root)
+    Process.wait pid
+    puts "Finished loading Pharos fixtures"
+  end
+
+  def pharos_start
+    if @pharos_pid == 0
+      env = env_hash
+      cmd = 'rbenv exec rails server'
+      log_file = "#{@log_dir}/pharos.log"
+      @pharos_pid = Process.spawn(env,
+                                  cmd,
+                                  chdir: @pharos_root,
+                                  out: [log_file, 'w'],
+                                  err: [log_file, 'w'])
+      Process.detach @pharos_pid
+      puts "Started Pharos with command '#{cmd}' and pid #{@pharos_pid}"
+    end
+  end
+
+  def pharos_stop
+    if @pharos_pid != 0
+      puts "Stopping Pharos (pid #{@pharos_pid})"
+      Process.kill('TERM', @pharos_pid)
+      @pharos_pid = 0
+    end
+  end
+
+  private
+
+  def start_go_service(name)
+    env = env_hash
+    cmd = "./#{name} -config #{@exchange_root}/config/integration.json"
+    pid = Process.spawn(env, cmd, chdir: @go_bin_dir)
+    Process.detach pid
+    puts "Started #{name} with command '#{cmd}' and pid #{pid}"
+    return pid
+  end
+
+  def stop_go_service(name, pid)
+      puts "Stopping #{name} service (pid #{pid})"
+      Process.kill('TERM', pid)
+  end
 
 end
 
 if __FILE__ == $0
   s = Service.new
-  #c.reset_pharos_db
-  #c.load_pharos_fixtures
+  s.apt_volume_service_start
+  s.apt_fetch_start
+  s.apt_record_start
+  s.apt_store_start
+  sleep 10
+  s.apt_volume_service_stop
+  s.apt_fetch_stop
+  s.apt_record_stop
+  s.apt_store_stop
 
-  s.start_dpn_cluster
-  sleep 30
-  s.stop_dpn_cluster
+
 end
