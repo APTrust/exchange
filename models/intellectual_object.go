30590c66895be43767c56a43f7bf3245b5266d17
A. Diamond <diamond@sheprador.com>
2016-12-27 15:47:04 -0500

    Added doc comments

--- models/intellectual_object.go
+++ models/intellectual_object.go
@@ -7,59 +7,186 @@ import (
 	"time"
 )
 
-/*
-IntellectualObject is Fluctus' version of an IntellectualObject.
-It belongs to an Institution and consists of one or more
-GenericFiles and a number of events.
-Institution is the owner of the intellectual object.
-Title is the title.
-Description is a free-text description of the object.
-Identifier is the object's unique identifier. (Whose assigned
-this id? APTrust or the owner?)
-Access indicate who can access the object. Valid values are
-consortial, institution and restricted.
-*/
+// IntellectualObject represents a single object (ingested bag) in APTrust.
+// An object can include any number of files and events.
+//
+// Properties are described below, but note that all of the "Ingest-"
+// fields populated and used by the Exchange ingest code on ingest only.
+// These fields are not stored in Pharos, and will not be populated on
+// any IntellectualObject retrieved from Pharos.
+//
+// Ingest services do save a JSON representation of IntellectualObjects,
+// including all of the "Ingest-" fields in the WorkItemState record
+// associated with the ingest WorkItem. That JSON record can be useful
+// for forensics, debugging, troubleshooting, and data reconstruction.
 type IntellectualObject struct {
+	// Id is the primary key id of this bag in Pharos.
 	// If Id is non-zero, this has been recorded in Pharos.
-	Id            int            `json:"id"`
-	Identifier    string         `json:"identifier"`
-	BagName       string         `json:"bag_name"`
-	Institution   string         `json:"institution"`
-	InstitutionId int            `json:"institution_id"`
-	Title         string         `json:"title"`
-	Description   string         `json:"description"`
-	Access        string         `json:"access"`
-	AltIdentifier string         `json:"alt_identifier"`
-	DPNUUID       string         `json:"dpn_uuid"`
-	GenericFiles  []*GenericFile `json:"generic_files"`
-	PremisEvents  []*PremisEvent `json:"events"`
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
+	Id int `json:"id"`
 
-	// The following are fields populated and used by the Exchange
-	// ingest code on ingest only. These fields are not stored in
-	// Pharos, and will not be populated on any IntellectualObject
-	// retrieved from Pharos.
-	IngestS3Bucket               string         `json:"ingest_s3_bucket"`
-	IngestS3Key                  string         `json:"ingest_s3_key"`
-	IngestTarFilePath            string         `json:"ingest_tar_file_path"`
-	IngestUntarredPath           string         `json:"ingest_untarred_path"`
-	IngestSize                   int64          `json:"ingest_size"`
-	IngestRemoteMd5              string         `json:"ingest_remote_md5"`
-	IngestLocalMd5               string         `json:"ingest_local_md5"`
-	IngestMd5Verified            bool           `json:"ingest_md5_verified"`
-	IngestMd5Verifiable          bool           `json:"ingest_md5_verifiable"`
-	IngestManifests              []string       `json:"ingest_manifests"`
-	IngestTagManifests           []string       `json:"ingest_tag_manifests"`
-	IngestFilesIgnored           []string       `json:"ingest_files_ignored"`
-	IngestTags                   []*Tag         `json:"ingest_tags"`
-	IngestMissingFiles           []*MissingFile `json:"ingest_missing_files"`
-	IngestTopLevelDirNames       []string       `json:"ingest_top_level_dir_names"`
-	IngestErrorMessage           string         `json:"ingest_error_message"`
-	IngestDeletedFromReceivingAt time.Time      `json:"ingest_deleted_from_receiving_at"`
+	// Identifier is the unique bag identifier, which is a
+	// string in the format "institution_identifier/bag_name".
+	// Example: "virginia.edu/bag1234"
+	Identifier string `json:"identifier"`
 
+	// BagName is the name of the bag, without the institution
+	// identifier prefix. Example: "bag1234"
+	BagName string `json:"bag_name"`
+
+	// Institution is the institution identifier (the domain name)
+	// of the institution that owns this bag.
+	Institution string `json:"institution"`
+
+	// InstitutionId is the Id (in Pharos) of the institution
+	// that owns this bag.
+	InstitutionId int `json:"institution_id"`
+
+	// Title is the title of the IntellectualObject. For example,
+	// "Architectural Plans for Alderman Library, 1933"
+	Title string `json:"title"`
+
+	// Description is a description of the IntellectualObject.
+	// This comes from the Internal-Sender-Description field of the
+	// bag-info.txt file.
+	Description string `json:"description"`
+
+	// Access describes who can see this intellectual object.
+	// This is specified in the aptrust-info.txt file. See
+	// https://sites.google.com/a/aptrust.org/member-wiki/basic-operations/bagging
+	// for a description of access policies.
+	Access string `json:"access"`
+
+	// AltIdentifier is an alternate identifier for this bag. It comes from
+	// the Internal-Sender-Identifier field in the bag-info.txt file.
+	AltIdentifier string `json:"alt_identifier"`
+
+	// DPNUUID is the DPN identifier for this bag, which is a UUID.
+	// This field will be empty if the bag has not been pushed to DPN.
+	DPNUUID string `json:"dpn_uuid"`
+
+	// GenericFiles is a list of the files that make up this bag.
+	GenericFiles []*GenericFile `json:"generic_files"`
+
+	// PremisEvents is a list of PREMIS events associated with this
+	// IntellectualObject. That includes events such as ingest and
+	// identifier assignment. Note that most PREMIS events are associated
+	// with GenericFiles, so see GenericFile.PremisEvents as well.
+	PremisEvents []*PremisEvent `json:"events"`
+
+	// CreatedAt is the Pharos timestamp describing when this
+	// IntellectualObject was first recorded in our database.
+	// This is usually within minutes of the ingest event, after
+	// all files have been copied to long-term storage.
+	CreatedAt time.Time `json:"created_at"`
+
+	// UpdatedAt describes when this object was last updated in Pharos.
+	// If this timestamp differs from CreatedAt, it usually means the
+	// bag (or some part of it) was ingested a second time.
+	UpdatedAt time.Time `json:"updated_at"`
+
+	// IngestS3Bucket is the bucket to which the depositor uploaded
+	// this bag. We fetch it from there to a local staging area for
+	// processing.
+	IngestS3Bucket string `json:"ingest_s3_bucket"`
+
+	// IngestS3Key is the file name in the S3 receiving bucket. It
+	// should be the bag name plus a ".tar" extension.
+	IngestS3Key string `json:"ingest_s3_key"`
+
+	// IngestTarFilePath is the absolute path to the tarred bag in
+	// our local staging area. We download the bag (as a tar file) from
+	// the receiving bucket to this local file.
+	IngestTarFilePath string `json:"ingest_tar_file_path"`
+
+	// IngestUntarredPath is the path to the untarred bag in our local
+	// staging area. This may be an empty string if we did not untar the bag.
+	// As of APTrust 2.0, we generally validate bags and send their files
+	// to long-term storage without ever untarring them.
+	IngestUntarredPath string `json:"ingest_untarred_path"`
+
+	// IngestSize is the size of the tarred bag we're trying to ingest.
+	IngestSize int64 `json:"ingest_size"`
+
+	// IngestRemoteMd5 is the etag of this bag as reported by the
+	// depositor's S3 receiving bucket. We use this to verify the download,
+	// if possible. For smaller bags (< 5GB), the etag is an md5 checksum.
+	// Large bags that the depositor sent to S3 via multipart
+	// upload have an etag that is calculated differently from a normal
+	// md5 checksum and includes a dash, followed by the number of parts
+	// in the original multipart upload. We cannot use those multipart
+	// etags for md5 validation.
+	IngestRemoteMd5 string `json:"ingest_remote_md5"`
+
+	// IngestLocalMd5 is the md5 digest of the tarred bag that we calculated
+	// locally upon downloading the file.
+	IngestLocalMd5 string `json:"ingest_local_md5"`
+
+	// IngestMd5Verified indicates whether or not we were able to verify
+	// the md5 digest of the entire bag upon download to our staging area.
+	IngestMd5Verified bool `json:"ingest_md5_verified"`
+
+	// IngestMd5Verifiable indicates whether we can verify our local md5
+	// digest against the S3 etag for this tarred bag. We cannot verify
+	// the checksum of large bags. See the comments on IngestRemoteMd5
+	// above.
+	IngestMd5Verifiable bool `json:"ingest_md5_verifiable"`
+
+	// IngestManifests is list of manifest files found inside this bag
+	// when we downloaded it.
+	IngestManifests []string `json:"ingest_manifests"`
+
+	// IngestTagManifests is a list of tag manifests found inside this
+	// bag when we downloaded it.
+	IngestTagManifests []string `json:"ingest_tag_manifests"`
+
+	// IngestFilesIgnored is a list of files found in the bag that are
+	// neither manifests, tag files, or data files. This includes files
+	// beginning with a dot (.) or dash (-). We do not save these files
+	// to long-term storage.
+	IngestFilesIgnored []string `json:"ingest_files_ignored"`
+
+	// IngestTags is a list of tags found in all of the tag files that
+	// we parsed when ingesting this bag. We parse only those tag files
+	// listed with the ParseAsTagFile option in
+	// config/aptrust_bag_validation_config.json. While Pharos itself
+	// only keeps and exposes the Title, Description, and Access tags,
+	// the JSON ingest data preserved in the WorkItemState record for
+	// each ingest includes a record of all tags parsed on ingest for
+	// items ingested in APTrust 2.0.
+	IngestTags []*Tag `json:"ingest_tags"`
+
+	// IngestMissingFiles is a list of files that appear in the bag's
+	// manifest(s) but were not found inside the tarred bag. This list
+	// should be empty for valid bags. This field is for reporting
+	// bag validation errors.
+	IngestMissingFiles []*MissingFile `json:"ingest_missing_files"`
+
+	// IngestTopLevelDirNames is a list of directory names found at
+	// the top of the directory hierarchy inside the tarred bag. The
+	// APTrust spec says there should be only one directory at the top
+	// level of the tar file contents, and that directory should have
+	// the same name as the bag, minus the ".tar" extension. This field
+	// is used for reporting bag validation errors.
+	IngestTopLevelDirNames []string `json:"ingest_top_level_dir_names"`
+
+	// IngestErrorMessage contains information about why ingest failed
+	// for this bag. On successful ingest, this field will be empty.
+	IngestErrorMessage string `json:"ingest_error_message"`
+
+	// IngestDeletedFromReceivingAt is a timestamp describing when the
+	// original tar file was deleted from the receiving bucket. After
+	// successful ingest, the workers/apt_recorder process should
+	// delete the tar file. If this timestamp is empty, it means the
+	// cleanup didn't happen, and we may be accumulating unneeded bags
+	// and incurring unnecessary costs in the receiving buckets.
+	IngestDeletedFromReceivingAt time.Time `json:"ingest_deleted_from_receiving_at"`
+
+	// genericFileMap is used internally to quickly find GenericFiles by
+	// their path within the bag. E.g. "data/photos/image1.jpg".
 	genericFileMap map[string]*GenericFile
-	tagMap         map[string][]*Tag
+
+	// tagMap is used internally to find tags by name.
+	tagMap map[string][]*Tag
 }
 
 func NewIntellectualObject() *IntellectualObject {
