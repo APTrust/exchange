46e0120501e4c36853b29d28d742420ce046f71d
A. Diamond <diamond@sheprador.com>
2017-01-06 11:29:16 -0500

    Added documention for WorkItem and WorkItemState fields

--- models/work_item.go
+++ models/work_item.go
@@ -28,33 +28,118 @@ import (
 // Status may have one of the following values: Pending,
 // Success, Failed.
 type WorkItem struct {
-	Id                    int        `json:"id"`
-	ObjectIdentifier      string     `json:"object_identifier"`
-	GenericFileIdentifier string     `json:"generic_file_identifier"`
-	Name                  string     `json:"name"`
-	Bucket                string     `json:"bucket"`
-	ETag                  string     `json:"etag"`
-	Size                  int64      `json:"size"`
-	BagDate               time.Time  `json:"bag_date"`
-	InstitutionId         int        `json:"institution_id"`
-	WorkItemStateId       *int       `json:"work_item_state_id"`
-	User                  string     `json:"user"`
-	Date                  time.Time  `json:"date"`
-	Note                  string     `json:"note"`
-	Action                string     `json:"action"`
-	Stage                 string     `json:"stage"`
-	StageStartedAt        *time.Time `json:"stage_started_at"`
-	Status                string     `json:"status"`
-	Outcome               string     `json:"outcome"`
-	Retry                 bool       `json:"retry"`
-	Node                  string     `json:"node"`
-	Pid                   int        `json:"pid"`
-	NeedsAdminReview      bool       `json:"needs_admin_review"`
-	// QueuedAt is a nullable DateTime in the Rails app,
-	// so it has to be a pointer here.
-	QueuedAt  *time.Time `json:"queued_at"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
+	// Id is the unique identifier for this work item.
+	Id int `json:"id"`
+	// ObjectIdentifier is the identifier of the IntellectualObject
+	// that is the subject of this WorkItem. E.g. "virginia.edu/bag1234".
+	// ObjectIdentifier will be empty until an item is ingested.
+	// ObjectIdentifier should be calculated as institution identifier,
+	// plus a slash, plus Name (see below) without the ".tar" extension.
+	// E.g. "virginia.edu/bag1234"
+	ObjectIdentifier string `json:"object_identifier"`
+	// GenericFileIdentifier is the identifier of the GenericFile to
+	// which this WorkItem pertains. This will be empty for WorkItems
+	// that only make sense at the object level, such as Ingest
+	// and Send-to-DPN. If GenericFileIdentifier is non-empty, it
+	// means the work for this WorkItem is to be performed on the
+	// GenericFile and not the object. For example, file deletion
+	// and fixity checking are performed on GenericFiles, not
+	// IntellectualObjects.
+	GenericFileIdentifier string `json:"generic_file_identifier"`
+	// Name is the name of the S3 key in the receiving bucket where
+	// this object first appeared. It should match the bag name, minus
+	// the institution prefix, with a ".tar" extension at the end.
+	// For example, if the IntellectualObject is "virginia.edu/bag1234",
+	// Name should be "bag1234.tar".
+	Name string `json:"name"`
+	// Bucket is the S3 receiving bucket to which this item was uploaded.
+	Bucket string `json:"bucket"`
+	// ETag is the S3 etag associated with this item. When a depositor
+	// uploads a new version of an existing bag, you'll find a new
+	// WorkItem record with the same Name and Bucket, but a different
+	// ETag.
+	ETag string `json:"etag"`
+	// Size is the size, in bytes, of the tar file in the S3 receiving
+	// bucket. This may not match the size of the restored bag because:
+	// 1) on restoration, we add a sha256 manifest if it didn't exist
+	// in the original bag, and 2) the depositor may have deleted some
+	// files from the IntellectualObject before restoring the bag.
+	Size int64 `json:"size"`
+	// BagDate is the creation timestamp on the bag, as reported by S3.
+	// This should be the date and time that the depositor created the
+	// bag (not the date/time when S3 received it).
+	BagDate time.Time `json:"bag_date"`
+	// InstitutionId is the unique identifier of the institution to
+	// whom this bag belongs.
+	InstitutionId int `json:"institution_id"`
+	// WorkItemStateId is the unique id of the WorkItemState record that
+	// contains JSON data describing the last known state of processing
+	// on this WorkItem. If processing has not started yet for this item,
+	// it will have no WorkItemState.
+	WorkItemStateId *int `json:"work_item_state_id"`
+	// User is the email address of the user who requested this WorkItem.
+	// For Ingest, this will always be the system user. For restoration,
+	// deletion and send-to-DPN requests, it will be the email address of
+	// the user who clicked the button or submitted the API request to
+	// start the process.
+	User string `json:"user"`
+	// Date ??
+	Date time.Time `json:"date"`
+	// Note is a human-readable note about the status of this WorkItem.
+	// This note is intended for users checking on the state of their
+	// work items, so it should be descriptive.
+	Note string `json:"note"`
+	// Action is the action to be performed in this WorkItem. See
+	// constants.ActionTypes.
+	Action string `json:"action"`
+	// Stage is the current stage of processing for this item. See.
+	// constants.StageTypes.
+	Stage string `json:"stage"`
+	// StageStartedAt describes when processing started for the current
+	// stage. If it's empty, processing for the current stage has not
+	// begun.
+	StageStartedAt *time.Time `json:"stage_started_at"`
+	// Status is the status of this WorkItem. See the values in
+	// constants.StatusTypes.
+	Status string `json:"status"`
+	// Outcome describes the outcome of a completed WorkItem. For example,
+	// Success, Failure, Cancelled.
+	Outcome string `json:"outcome"`
+	// Retry indicates whether or not a failed or uncompleted WorkItem
+	// should be retried. This will be set to false if processing resulted
+	// in a fatal error (such as when trying to ingest an invalid bag) or
+	// when processing has encountered too many transient errors (such as
+	// network connection problems, lack of disk space, etc.). The threshold
+	// for transient errors is defined by the MaxAttempts config setting for
+	// each WorkerConfig. WorkItems that fail due to repeated transient errors
+	// should be requeued by an administrator when the cause of the transient
+	// error resolves. Part of the requeuing process involves setting Retry
+	// back to true.
+	Retry bool `json:"retry"`
+	// Node is the hostname or IP address of the machine that is currently
+	// processing this request. The worker process sets this field when it
+	// begins processing and clears it when it's done. If a worker crashes,
+	// you can identify orphaned WorkItems by the combination of non-empty
+	// Node and StageStartedAt. You can check Pid as well, to see if it's
+	// still running on Node.
+	Node string `json:"node"`
+	// Pid is the process id of the worker currently handling this WorkItem.
+	// Workers set and clear Pid in the same way they set and clear Node.
+	Pid int `json:"pid"`
+	// NeedsAdminReview indicates whether an administrator needs to look into
+	// this WorkItem. The worker process that attepts to fulfill this WorkItem
+	// will set this to true when it encounters unexpected errors.
+	NeedsAdminReview bool `json:"needs_admin_review"`
+	// QueuedAt describes when this item was copied into NSQ. This is a nullable
+	// DateTime in the Rails app, so it has to be a pointer here. When requeuing
+	// an item, set this to nil and set Retry to true. Otherwise, apt_queue will
+	// ignore it. QueuedAt exists to prevent apt_queue from adding items more than
+	// once to an NSQ topic.
+	QueuedAt *time.Time `json:"queued_at"`
+	// CreatedAt is the Rails timestamp describing when this item was created.
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt is the Rails timestamp describing when this item was updated.
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // Convert WorkItem to JSON, omitting id and other attributes that
--- models/work_item_state.go
+++ models/work_item_state.go
@@ -7,13 +7,41 @@ import (
 	"time"
 )
 
+// WorkItemState contains information about what work has been completed,
+// and what work remains to be done, for the associated WorkItem. WorkItems
+// that have not yet started processing will have no associated WorkItemState.
 type WorkItemState struct {
-	Id         int       `json:"id"`
-	WorkItemId int       `json:"work_item_id"`
-	Action     string    `json:"action"`
-	State      string    `json:"state"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	// Id is the unique identifier for this WorkItemState object.
+	Id int `json:"id"`
+	// WorkItemId is the unique identifier of the WorkItem whose state
+	// this object describes.
+	WorkItemId int `json:"work_item_id"`
+	// Action is the WorkItem action to be performed. See constants.ActionTypes.
+	Action string `json:"action"`
+	// State is a JSON string describing the state of processing, what work has
+	// been completed, and what work remains to be done. This JSON string
+	// deserializes to different types, based on the Action. For example, if
+	// Action is "Ingest", the JSON deserializes to in IngestState object.
+	// The workers in the /workers directory retrieve this state when they begin
+	// work an a WorkItem and update it when they stop work on that item.
+	//
+	// It's common for long-running tasks to fail due to network errors. For
+	// example, copying 10,000 files from a large bag to S3 and/or Glacier
+	// may fail half-way through due to connectivity problems. When that
+	// happens, the worker will stop work on the item and preserve its state
+	// in JSON format in this field. The next worker to pick up the task will
+	// deserialize the JSON state info, see that the first 5000 files were
+	// already successfully stored, and then resume the storage work at file
+	// #5001.
+	//
+	// This state information is essential for intelligently resuming work
+	// after failures, and for forensics on failed items. Admin users can
+	// see the state JSON in the WorkItem detail view in Pharos.
+	State string `json:"state"`
+	// CreatedAt is the Rails timestamp describing when this item was created.
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt is the Rails timestamp describing when this item was updated.
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func NewWorkItemState(workItemId int, action, state string) *WorkItemState {
