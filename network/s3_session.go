452f267e8aae7a1150ba40f4c2629ad9eab87bce
A. Diamond <diamond@sheprador.com>
2017-04-26 17:49:15 -0400

    Don't panic if AWS env vars are not set

--- network/s3_session.go
+++ network/s3_session.go
@@ -1,20 +1,14 @@
 package network
 
 import (
-	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"os"
 )
 
 // Returns an S3 session for this objectList.
 func GetS3Session(awsRegion, accessKeyId, secretAccessKey string) (*session.Session, error) {
-	testsAreRunning := flag.Lookup("test.v") != nil
-	if !testsAreRunning && (os.Getenv("AWS_ACCESS_KEY_ID") == "" || os.Getenv("AWS_SECRET_ACCESS_KEY") == "") {
-		panic("AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY not set in environment")
-	}
 	creds := credentials.NewEnvCredentials()
 	if accessKeyId != "" && secretAccessKey != "" {
 		creds = credentials.NewStaticCredentials(accessKeyId, secretAccessKey, "")
