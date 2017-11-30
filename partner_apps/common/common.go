f09ad7c39a3a1d6e3cbb8c5ebb08e405983091ad
A. Diamond <diamond@sheprador.com>
2017-11-30 11:51:19 -0500

    Added build script for partner apps

---
+++ partner_apps/common/common.go
@@ -0,0 +1,61 @@
+package common
+
+import (
+//"fmt"
+//"runtime"
+//"strings"
+)
+
+// These vars are set by the build script in scripts/build_partner_tools.rb
+var (
+	Version   string
+	BuildDate string
+	GitHash   string
+	License   string
+	WikiUrl   string
+	RepoUrl   string
+	Email     string
+)
+
+// Exit Codes
+const (
+	// EXIT_OK means program completed successfully.
+	EXIT_OK = 0
+	// EXIT_RUNTIME_ERR means program did not complete
+	// successfully due to an error. The error may have
+	// occurred outside the program, such as a network
+	// error or an error on a remote server.
+	EXIT_RUNTIME_ERR = 1
+	// EXIT_BAG_INVALID is used primarily for apt_validate.
+	// It means the program completed its run and found that
+	// the bag is not valid.
+	EXIT_BAG_INVALID = 2
+	// EXIT_USER_ERR means the user did not supply some
+	// required option or argument, or the user supplied
+	// invalid options/arguments.
+	EXIT_USER_ERR = 3
+	// EXIT_ITEM_NOT_FOUND can occur when the user tries to
+	// upload, download, or validate a file that doesn't exist,
+	// or when apt_check_ingest is asked to check on a bag
+	// which has no record in Pharos.
+	EXIT_ITEM_NOT_FOUND = 4
+	// EXIT_NOT_INGESTED occurs when apt_check_ingest finds
+	// a WorkItem record for a bag in Pharos, but the bag has
+	// not yet been fully ingested.
+	EXIT_NOT_INGESTED = 5
+	// EXIT_SOME_INGESTED occurs when apt_check_ingest finds
+	// multiple WorkItems in Pharos matching a bag name, and
+	// some versions of the bag have been ingested while others
+	// have not.
+	EXIT_SOME_INGESTED = 6
+	// EXIT_NO_OP means the user requested help message or
+	// version info. The program printed the info, and no other
+	// operations were performed.
+	EXIT_NO_OP = 100
+)
+
+func GetVersion() {
+	//osName := strings.Title(runtime.GOOS)
+	//architecture := runtime.GOARCH
+
+}
---
+++ scripts/build_partner_tools.rb
@@ -0,0 +1,110 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+# Don't include spaces in these vars, since they cause
+# problems in the linker, even when quoted.
+REPO_URL = "https://github.com/APTrust/exchange"
+WIKI_URL = "https://wiki.aptrust.org/Partner_Tools"
+LICENSE = "Apache-2.0"
+EMAIL = "support@aptrust.org"
+
+@apps = ['apt_check_ingest',
+        'apt_delete',
+        'apt_download',
+        'apt_list',
+        'apt_upload',
+        'apt_validate']
+
+def run()
+  options = parse_options()
+  if !options['version'] || !options['platform']
+    exit 1
+  end
+  git_hash = `git rev-parse --short HEAD`.chomp
+  build_date = Time.now.utc.strftime("%FT%T%:z")
+  ldflags = get_ld_flags(options['version'], build_date, git_hash)
+  tags = "-tags='partners,#{options['platform']}'"
+  build_dir = ensure_build_dir(options['exchange_root'])
+  @apps.each do |app_name|
+    source_dir = File.join(options['exchange_root'], 'partner_apps', app_name)
+    cmd = "go build #{tags} #{ldflags} -o #{build_dir}/#{app_name} #{app_name}.go"
+    puts "cd #{source_dir}"
+    puts cmd
+    pid = Process.spawn(cmd, chdir: source_dir)
+    Process.wait pid
+    if $?.exitstatus != 0
+      raise "Build failed for #{app_name}"
+    end
+  end
+end
+
+def get_ld_flags(version, build_date, git_hash)
+  pkg = "github.com/APTrust/exchange/partner_apps/common/common"
+  flags =  [ "-X #{pkg}.Version=#{version}",
+             "-X #{pkg}.BuildDate=#{build_date}",
+             "-X #{pkg}.GitHash=#{git_hash}",
+             "-X #{pkg}.RepoUrl=#{REPO_URL}",
+             "-X #{pkg}.WikiUrl=#{WIKI_URL}",
+             "-X #{pkg}.License=#{LICENSE}",
+             "-X #{pkg}.Email=#{EMAIL}"
+           ]
+  return "-ldflags \"#{flags.join(' ')}\""
+end
+
+def ensure_build_dir(exchange_root)
+  build_dir = File.join(exchange_root, 'partner_apps', 'bin')
+  if !Dir.exist?(build_dir)
+    puts "Creating build directory #{build_dir}"
+    Dir.mkdir(build_dir)
+  end
+  return build_dir
+end
+
+def parse_options()
+  options = {}
+  OptionParser.new do |opts|
+    opts.on("-h", "--help", print_help) do
+      print_help
+      exit!
+    end
+    opts.on("-p", "--platform platform", "Build for platform (mac, linux, windows)") do |p|
+      puts "Platform = #{p}"
+      if !['mac', 'linux', 'windows'].include?(p)
+        puts "Required option -p must be one of 'mac', 'linux', or 'windows'"
+        exit!
+      end
+      options['platform'] = p
+    end
+    opts.on("-v", "--version version", "Build version to assign to binaries (e.g. 2.3-beta)") do |v|
+      puts "Version = #{v}"
+      if v.nil? || v == ""
+        puts "Option -v (--version) is required. E.g. --version=2.3-beta"
+        exit!
+      end
+      options['version'] = v
+    end
+  end.parse!
+  options['exchange_root'] = ENV['EXCHANGE_ROOT'] || abort("Set env var EXCHANGE_ROOT")
+  return options
+end
+
+def print_help()
+  puts ""
+  puts "build_partner_tools.rb --platform=< mac | linux | windows> --version=<version>"
+  puts "Builds and packages all partner tools"
+  puts "Option --p or --platform is the platform for which you are building."
+  puts "Option --v or --version is the version number to stamp on this build."
+  puts ""
+  puts "Requires environment variable EXCHANGE_ROOT, which should be set to "
+  puts "the directory containing the exchange source code."
+  puts ""
+end
+
+if __FILE__ == $0
+  run()
+end
+
+
+#cd apt_check_ingest
+#go build -tags=partners -ldflags "-X =`date -u +.%Y%m%d.%H%M%S`" service.go
