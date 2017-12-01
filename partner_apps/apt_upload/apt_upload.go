b2b96701b233f0e78f02715e45464d97177b7ce7
A. Diamond <diamond@sheprador.com>
2017-12-01 11:12:23 -0500

    Updated doc and standardized exit codes in apt_upload

--- partner_apps/apt_upload/apt_upload.go
+++ partner_apps/apt_upload/apt_upload.go
@@ -13,20 +13,11 @@ import (
 	"strings"
 )
 
-const (
-	EXIT_OK          = 0 // Item was successfully uploaded.
-	EXIT_FAILED      = 1 // Upload failed.
-	EXIT_NOT_EXISTS  = 2 // File does not exist.
-	EXIT_USER_ERR    = 3 // Operation could not be completed due to usage error (e.g. missing params)
-	EXIT_RUNTIME_ERR = 4 // Operation could not be completed due to runtime, network, or server error
-	EXIT_HELP        = 5 // Printed help or version message. No other operations attempted.
-)
-
 func main() {
 	opts := getUserOptions()
 	if opts.HasErrors() {
 		fmt.Fprintln(os.Stderr, opts.AllErrorsAsString())
-		os.Exit(EXIT_USER_ERR)
+		os.Exit(common.EXIT_USER_ERR)
 	}
 	uploadClient := network.NewS3Upload(
 		opts.AccessKeyId,
@@ -58,9 +49,9 @@ func exitOnFileError(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		if os.IsNotExist(err) {
-			os.Exit(EXIT_NOT_EXISTS)
+			os.Exit(common.EXIT_ITEM_NOT_FOUND)
 		}
-		os.Exit(EXIT_RUNTIME_ERR)
+		os.Exit(common.EXIT_RUNTIME_ERR)
 	}
 }
 
@@ -80,14 +71,14 @@ func printResult(opts *common.Options, uploadClient *network.S3Upload, filesize 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, result.ToText())
 			fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(EXIT_RUNTIME_ERR)
+			os.Exit(common.EXIT_RUNTIME_ERR)
 		}
 	}
 	fmt.Println(output)
 	if result.ErrorMessage != "" {
-		return EXIT_RUNTIME_ERR
+		return common.EXIT_RUNTIME_ERR
 	}
-	return EXIT_OK
+	return common.EXIT_OK
 }
 
 // Get user-specified options from the command line,
@@ -123,16 +114,16 @@ func parseCommandLine() *common.Options {
 
 	if version {
 		fmt.Println(common.GetVersion())
-		os.Exit(EXIT_HELP)
+		os.Exit(common.EXIT_NO_OP)
 	}
 	if help {
 		printUsage()
-		os.Exit(EXIT_HELP)
+		os.Exit(common.EXIT_NO_OP)
 	}
 
 	if len(flag.Args()) < 1 {
 		fmt.Fprintln(os.Stderr, "Please specify a file to upload.")
-		os.Exit(EXIT_USER_ERR)
+		os.Exit(common.EXIT_USER_ERR)
 	}
 
 	filePath, err := filepath.Abs(flag.Arg(0))
@@ -165,7 +156,7 @@ func parseCommandLine() *common.Options {
 		err := json.Unmarshal([]byte(metadata), &meta)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Cannot parse metadata JSON:", err)
-			os.Exit(EXIT_RUNTIME_ERR)
+			os.Exit(common.EXIT_RUNTIME_ERR)
 		}
 		opts.Metadata = meta
 	}
@@ -182,17 +173,23 @@ Usage:
 
 apt_upload [options] <file>
 
-apt_upload -config=<path to config file> \
-		   -region=<aws region to connect to> \
-		   -bucket=<bucket to upload to> \
-		   -key=<name/key of object to upload> \
-		   -contentType=<mime type of upload> \
-		   -format=<'text' or 'json'> \
-		   -metadata=<json string> \
-           [-version] \
+apt_upload --bucket=<bucket to upload to> \
+           [--config=<path to config file>] \
+		   [--contentType=<mime type of upload>] \
+		   [--format=<'text' or 'json'>] \
+		   [--region=<aws region to connect to>] \
+		   [--key=<name/key of object to upload>] \
+		   [--metadata=<json string>] \
 		   <file>
 
-Params:
+apt_upload --help
+
+apt_upload --version
+
+Options:
+
+Note that option flags may be preceded by either one or two dashes,
+so -option is the same as --option.
 
 Note that file is the only required param. This program will get your
 AWS credentials from the config file, if it can find one. Otherwise,
@@ -200,50 +197,53 @@ it will get your AWS credentials from the environment variables
 "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY". If it can't find your
 AWS credentials, the upload will fail.
 
--config is the optional path to your APTrust partner config file.
-		If you omit this, the uploader uses the config at
-		~/.aptrust_partner.conf (Mac/Linux) or %HOMEPATH%\.aptrust_partner.conf
-		(Windows) if that file exists. The config file should contain
-		your AWS keys, and the locations of your receiving bucket.
-		For info about what should be in your config file, see
-		https://wiki.aptrust.org/Partner_Tools
+--config is the optional path to your APTrust partner config file.
+  If you omit this, the uploader uses the config at
+  ~/.aptrust_partner.conf (Mac/Linux) or %HOMEPATH%\.aptrust_partner.conf
+  (Windows) if that file exists. The config file should contain
+  your AWS keys, and the locations of your receiving bucket.
+  For info about what should be in your config file, see
+  https://wiki.aptrust.org/Partner_Tools
 
--region is the S3 region to connect to. This defaults to us-east-1. You
-		generally should not have to set this for APTrust uploads,
-		but you may set it on the command line to upload non-APTrust
-		files from your own buckets.
+--region is the S3 region to connect to. This defaults to us-east-1. You
+  generally should not have to set this for APTrust uploads,
+  but you may set it on the command line to upload non-APTrust
+  files from your own buckets.
 
--bucket is the name of the S3 bucket to upload to. If this is not
-		specified on the command line, apt_upload will use the
-		restoration bucket specified in your APTrust partner config file.
-		See the -config option for more info.
+--bucket is the name of the S3 bucket to upload to. If this is not
+  specified on the command line, apt_upload will use the
+  receiving bucket specified in your APTrust partner config file.
+  See the --config option for more info.
 
--key    if you want your uploaded file to have a different name in S3,
-		specify that here. If you upload a file from /home/joy/my_file.txt,
-		it will be put into your S3 bucket with the name "my_file.txt".
-		Setting the -key option allows you to override that. So if
-		-key='file_001.txt', /home/joy/my_file.txt will be saved to your
-		S3 bucket with the name file_001.txt.
+--key allows you to specify the key name to be stored in S3. If you upload
+  a file from /home/joy/my_file.txt and don't specify the --key option,
+  your file will be put into your S3 bucket with the name "my_file.txt".
+  Setting the --key option allows you to override that. So if
+  -key='file_001.txt', /home/joy/my_file.txt will be saved to your
+  S3 bucket with the name file_001.txt.
 
--contentType is the optional content type of the file you're uploading.
-		If you choose to specify this, it should be in mime type format.
-		For example, "image/jpeg" or "text/plain". You typically don't
-		need to set this. If left unset, this usually defaults to something
-		generic and unhelpful like "application/octet-stream".
-		If you want to set it, you'll find a full list of mime types at
-		https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+--contentType is the optional content type of the file you're uploading.
+  If you choose to specify this, it should be in mime type format.
+  For example, "image/jpeg" or "text/plain". You typically don't
+  need to set this. If left unset, this usually defaults to something
+  generic and unhelpful like "application/octet-stream".
+  If you want to set it, you'll find a full list of mime types at
+  https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
 
--format is the format of the output printed to STDOUT when the upload
-		is complete. Options are 'text' and 'json', and the default is
-		'text'.
+--format is the format of the output printed to STDOUT when the upload
+  is complete. Options are 'text' and 'json', and the default is 'text'.
 
--metadata allows you to specify optional metadata, in json format, to be
-		saved in S3 with your file. A metadata json string should look
-		like this:
+--metadata allows you to specify optional metadata, in json format, to be
+  saved in S3 with your file. A metadata json string should look
+  like this:
 
-		-metadata='{"Bag":"my_bag","Bagpath":"data/Image001.tif","Institution":"virginia.edu","Md5":"12345","Sha256":"54321"}'
+  -metadata='{"Bag":"my_bag","Bagpath":"data/Image001.tif",
+              "Institution":"virginia.edu","Md5":"12345",
+              "Sha256":"54321"}'
 
--version prints version info and exits.
+--version prints version info and exits.
+
+--help prints this help message and exits.
 
 Examples:
 
@@ -256,21 +256,20 @@ Examples:
 2. Upload item "/home/joy/my_bag.tar" to your receiving bucket, using a
    custom APTrust partner config file
 
-   apt_upload -config="/home/joy/aptrust_config.txt" /home/joy/my_bag.tar
+   apt_upload --config="/home/joy/aptrust_config.txt" /home/joy/my_bag.tar
 
 3. Upload item "/home/joy/my_bag.tar" to a specified bucket with a custom
    name
 
-   apt_upload -bucket="my.custom.bucket" -key="MySpecialFile.tar" /home/joy/my_bag.tar
+   apt_upload --bucket="my.custom.bucket" --key="MySpecialFile.tar" /home/joy/my_bag.tar
 
 Exit codes:
 
 0 - Item was successfully uploaded.
 1 - Upload failed.
-2 - File does not exist.
 3 - Operation could not be completed due to usage error (e.g. missing params)
-4 - Operation could not be completed due to runtime, network, or server error
-5 - Printed help or version message. No other operations attempted.
+4 - File does not exist.
+100 - Printed help or version message. No other operations attempted.
 `
 	fmt.Println(message)
 }
