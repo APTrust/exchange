22300f0ce639c17039b3f38d2190f04339ed5752
A. Diamond <diamond@sheprador.com>
2017-12-01 10:02:41 -0500

    Updated doc and standardized exit codes in apt_delete

--- partner_apps/apt_download/apt_download.go
+++ partner_apps/apt_download/apt_download.go
@@ -11,21 +11,11 @@ import (
 	"strings"
 )
 
-const (
-	EXIT_OK          = 0 // Bag was successfully downloaded.
-	EXIT_FAILED      = 1 // Bag was found in S3 but download failed.
-	EXIT_NOT_FOUND   = 2 // Bag was not found in S3.
-	EXIT_USER_ERR    = 3 // Operation could not be completed due to usage error (e.g. missing params)
-	EXIT_RUNTIME_ERR = 4 // Operation could not be completed due to runtime, network, or server error
-	EXIT_HELP        = 5 // Printed help or version message. No other operations attempted.
-
-)
-
 func main() {
 	opts := getUserOptions()
 	if opts.HasErrors() {
 		fmt.Fprintln(os.Stderr, opts.AllErrorsAsString())
-		os.Exit(EXIT_USER_ERR)
+		os.Exit(common.EXIT_USER_ERR)
 	}
 	client := network.NewS3Download(
 		opts.AccessKeyId,
@@ -46,15 +36,15 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, result.ToText())
 			fmt.Fprintf(os.Stderr, err.Error())
-			os.Exit(EXIT_RUNTIME_ERR)
+			os.Exit(common.EXIT_RUNTIME_ERR)
 		}
 	}
 	fmt.Println(output)
-	exitCode := EXIT_OK
+	exitCode := common.EXIT_OK
 	if strings.Contains(result.ErrorMessage, "NoSuchKey") {
-		exitCode = EXIT_NOT_FOUND
+		exitCode = common.EXIT_ITEM_NOT_FOUND
 	} else if result.ErrorMessage != "" {
-		exitCode = EXIT_RUNTIME_ERR
+		exitCode = common.EXIT_RUNTIME_ERR
 	}
 	os.Exit(exitCode)
 }
@@ -63,6 +53,9 @@ func main() {
 // environment, and/or config file.
 func getUserOptions() *common.Options {
 	opts := parseCommandLine()
+	// If no bucket specified, the call below will
+	// set the bucket to the restore bucket specified
+	// in the config file, if possible.
 	opts.SetAndVerifyDownloadOptions()
 	return opts
 }
@@ -90,11 +83,11 @@ func parseCommandLine() *common.Options {
 
 	if version {
 		fmt.Println(common.GetVersion())
-		os.Exit(EXIT_HELP)
+		os.Exit(common.EXIT_NO_OP)
 	}
 	if help {
 		printUsage()
-		os.Exit(EXIT_HELP)
+		os.Exit(common.EXIT_NO_OP)
 	}
 
 	opts := &common.Options{
@@ -124,82 +117,83 @@ apt_download downloads a file from S3 to a local directory
 
 Usage:
 
-apt_download -config=<path to config file> \
-			 -region=<aws region to connect to> \
-			 -bucket=<bucket to download from> \
-			 -key=<name/key of object to download> \
-			 -dir=<download the object to this dir> \
-             [-version] \
-			 -format=<'text' or 'json'>
+apt_download --key=<name/key of object to download> \
+             [--bucket=<bucket to download from>] \
+             [--config=<path to config file>] \
+             [--region=<aws region to connect to>] \
+			 [--dir=<download the object to this dir>] \
+			 [--format=<'text' or 'json'>]
 
-Params:
+apt_download --help
 
-Note that key is the only required param. This program will get
-your AWS credentials from the config file, if it can find one. Otherwise,
-it will get your AWS credentials from the environment variables
-"AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY". If it can't find your
-AWS credentials, the download will fail.
+apt_download --version
 
--config is the optional path to your APTrust partner config file.
-		If you omit this, the downloader uses the config at
-		~/.aptrust_partner.conf (Mac/Linux) or %HOMEPATH%\.aptrust_partner.conf
-		(Windows) if that file exists. The config file should contain
-		your AWS keys, and the locations of your receiving bucket, restore
-		bucket, and the local directory into which you want items downloaded.
-		For info about what should be in your config file, see
-		https://wiki.aptrust.org/Partner_Tools
+Options:
 
--region is the S3 region to connect to. This defaults to us-east-1. You
-		generally should not have to set this for APTrust downloads,
-		but you may set it on the command line to download non-APTrust
-		files from your own buckets.
+Note that option flags may be preceded by either one or two dashes,
+so -option is the same as --option.
 
--bucket is the name of the S3 bucket to download from. If omitted, the program
-		will use the name of the restoration bucket in your partner config
-		file. If you have no config file, or the restoration bucket isn't
-		specified there, then you must specify the bucket on the command line.
+This program will get your AWS credentials from the config file, if
+it can find one. Otherwise, it will get your AWS credentials from
+the environment variables "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY".
+If it can't find your AWS credentials, the download will fail.
 
--key    is the name of the item you want to download from S3. This param
-		is required.
+--bucket is the name of the S3 bucket to download from. If omitted, the program
+  will use the name of the restoration bucket in your partner config
+  file. If you have no config file, or the restoration bucket isn't
+  specified there, then you must specify the bucket on the command line.
 
--dir    is the directory into which you want to download the S3 file.
-		If this is not specified, and you have an APTrust partner config
-		file, apt_download will use the DownloadDir setting there. If
-		there's no config file, your S3 item will be downloaded into the
-		current working directory from which you're running this app.
+--config is the optional path to your APTrust partner config file.
+  If you omit this, the downloader uses the config at
+  ~/.aptrust_partner.conf (Mac/Linux) or %HOMEPATH%\.aptrust_partner.conf
+  (Windows) if that file exists. The config file should contain
+  your AWS keys, and the locations of your receiving bucket, restore
+  bucket, and the local directory into which you want items downloaded.
+  For info about what should be in your config file, see
+  https://wiki.aptrust.org/Partner_Tools
 
--format is the format of the output printed to STDOUT when the download
-		is complete. Options are 'text' and 'json', and the default is
-		'text'.
+--dir is the directory into which you want to download the S3 file.
+  If this is not specified, and you have an APTrust partner config
+  file, apt_download will use the DownloadDir setting there. If
+  there's no config file, your S3 item will be downloaded into the
+  current working directory from which you're running this app.
 
--version prints version info and exits.
+--format is the format of the output printed to STDOUT when the download
+  is complete. Options are 'text' and 'json', and the default is 'text'.
+
+--help prints this help message and exits.
+
+--key is the name of the item you want to download from S3. This param
+  is required.
+
+--region is the S3 region to connect to. This defaults to us-east-1. You
+  generally should not have to set this for APTrust downloads,
+  but you may set it on the command line to download non-APTrust
+  files from your own buckets.
+
+--version prints version info and exits.
 
 Examples:
 
-1. Download item "my_bag.tar" from your restoration bucket, using your
-   default APTrust partner config file in ~/.aptrust_partner.conf (Mac/Linux)
-   or %HOMEPATH%\.aptrust_partner.conf
+1. Download item "my_bag.tar" from your restoration bucket, using a
+   custom APTrust partner config file.
 
-   apt_download -bucket="aptrust.restore.test.edu" -key="my_bag.tar"
+   apt_download -bucket="aptrust.restore.test.edu" -key="my_bag.tar" -config="config.txt"
 
-2. Download item "my_bag.tar" from your restoration bucket, using a
-   custom APTrust partner config file
-
-   apt_download -bucket="aptrust.restore.test.edu" -key="my_bag.tar" -config="/home/joy/aptrust_config.txt"
-
-3. Download item "my_bag.tar" from a specified bucket and save it in
-   /home/joy/downloads
+2. Download item "my_bag.tar" from a specified bucket and save it in
+   /home/joy/downloads. Since there's no --config option here, this assumes
+   that the environment vars "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY"
+   are set.
 
    apt_download -key="my_bag.tar" -bucket="my.custom.bucket" -dir="/home/joy/downloads"
 
 Exit codes:
 
 0 - Bag was successfully downloaded.
-1 - Bag was found in S3 but download failed.
-2 - Bag was not found in S3.
+1 - Operation could not be completed due to runtime, network, or server error
 3 - Operation could not be completed due to usage error (e.g. missing params)
-4 - Operation could not be completed due to runtime, network, or server error
-5 - Printed help or version message. No other operations attempted.
+4 - Bag was not found in S3.
+100 - Printed help or version message. No other operations attempted.
 `
 	fmt.Println(message)
 }
