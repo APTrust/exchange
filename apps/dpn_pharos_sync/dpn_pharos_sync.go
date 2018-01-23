9832db84359a3c50338f3c542746086e623834f3
A. Diamond <diamond@sheprador.com>
2018-01-23 14:22:39 -0500

    Working on DPN-Pharos bag sync

--- apps/dpn_pharos_sync/dpn_pharos_sync.go
+++ apps/dpn_pharos_sync/dpn_pharos_sync.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"github.com/APTrust/exchange/context"
-	//"github.com/APTrust/exchange/dpn/network"
+	dpn_models "github.com/APTrust/exchange/dpn/models"
+	"github.com/APTrust/exchange/dpn/network"
 	"github.com/APTrust/exchange/models"
+	"net/url"
 	"os"
+	"time"
 )
 
 // dpn_sync syncs data in our local DPN registry by pulling data about
@@ -22,6 +25,7 @@ func main() {
 	_context := context.NewContext(config)
 	err = syncToPharos(_context)
 	if err != nil {
+		_context.MessageLog.Error(err.Error())
 		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(1)
 	}
@@ -29,28 +33,63 @@ func main() {
 
 // getLatestTimestamp returns the latest UpdatedAt timestamp
 // from the DPN bags table in Pharos.
-func getLatestTimestamp(ctx *context.Context) {
-	//ctx.PharosClient.
+func getLatestTimestamp(ctx *context.Context) (time.Time, error) {
+	params := url.Values{}
+	params.Add("sort", "dpn_upated_at DESC")
+	resp := ctx.PharosClient.DPNBagList(params)
+	if resp.Error != nil {
+		return time.Time{}, resp.Error
+	}
+	return resp.DPNBag().DPNUpdatedAt, nil
 }
 
 func syncToPharos(ctx *context.Context) error {
-	// get latest timestamp
-	// get all DPN bags updated since that timestamp
-	// for each bag:
-	//    convert to Pharos DPNBag record
-	//    save to Pharos
+	timestamp, err := getLatestTimestamp(ctx)
+	if err != nil {
+		return err
+	}
+	dpnClient, err := network.NewDPNRestClient(
+		ctx.Config.DPN.RestClient.LocalServiceURL,
+		ctx.Config.DPN.DPNAPIVersion,
+		ctx.Config.DPN.RestClient.LocalAuthToken,
+		ctx.Config.DPN.LocalNode,
+		ctx.Config.DPN)
+	if err != nil {
+		return fmt.Errorf("Error creating local DPN REST client: %v", err)
+	}
+	params := url.Values{}
+	params.Add("after", timestamp.Format(time.RFC3339))
+	params.Add("page", "1")
+	params.Add("page_size", "100")
 
-	// localClient, err := network.NewDPNRestClient(
-	// 	ctx.Config.DPN.RestClient.LocalServiceURL,
-	// 	ctx.Config.DPN.DPNAPIVersion,
-	// 	ctx.Config.DPN.RestClient.LocalAuthToken,
-	// 	ctx.Config.DPN.LocalNode,
-	// 	ctx.Config.DPN)
-	// if err != nil {
-	// 	return fmt.Errorf("Error creating local DPN REST client: %v", err)
-	// }
+	for {
+		resp := dpnClient.DPNBagList(params)
+		if resp.Error != nil {
+			return resp.Error
+		}
+		for _, bag := range resp.Bags() {
+			pharosDPNBag := convertToPharos(bag)
+			saveResponse := ctx.PharosClient.DPNBagSave(pharosDPNBag)
+			if saveResponse.Error != nil {
+				ctx.MessageLog.Error("Error saving DPN Bag %s to Pharos: %v",
+					bag.UUID, saveResponse.Error)
+			} else {
+				ctx.MessageLog.Info("Saved DPN Bag %s with id %d",
+					bag.UUID, saveResponse.DPNBag().Id)
+			}
+		}
+		if !resp.HasNextPage() {
+			break
+		} else {
+			params = resp.ParamsForNextPage()
+		}
+	}
 
 	return nil
+}
+
+func convertToPharos(dpnBag *dpn_models.DPNBag) *models.PharosDPNBag {
+	return &models.PharosDPNBag{}
 }
 
 // See if you can figure out from the function name what this does.
