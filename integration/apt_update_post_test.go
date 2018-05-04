be2b3208bdc8e38365122288a70f50a5e65ab600
A. Diamond <diamond@sheprador.com>
2018-05-04 15:32:56 -0400

    Started ingest-updated post tests for Glacier-only items. Currently not passing.

--- integration/apt_update_post_test.go
+++ integration/apt_update_post_test.go
@@ -34,6 +34,9 @@ import (
 const (
 	UPDATED_BAG_IDENTIFIER = "test.edu/example.edu.tagsample_good"
 	UPDATED_BAG_ETAG       = "a08e2b0b3ba3490434c23a8d78b1760f"
+
+	UPDATED_GLACIER_BAG_IDENTIFIER = "test.edu/example.edu.sample_glacier_oh"
+	UPDATED_GLACIER_BAG_ETAG       = "bf01126663915a4f5d135a37443b8349"
 )
 
 func TestUpdatedItemsInPharos(t *testing.T) {
@@ -42,15 +45,17 @@ func TestUpdatedItemsInPharos(t *testing.T) {
 	}
 	_context, err := testutil.GetContext("integration.json")
 	require.Nil(t, err)
-	testUpdatedWorkItem(t, _context)
+	testUpdatedWorkItem(t, _context, UPDATED_BAG_IDENTIFIER, UPDATED_BAG_ETAG)
+	testUpdatedWorkItem(t, _context, UPDATED_GLACIER_BAG_IDENTIFIER, UPDATED_GLACIER_BAG_ETAG)
 	testUpdatedObject(t, _context)
+	testUpdatedGlacierObject(t, _context)
 }
 
-func testUpdatedWorkItem(t *testing.T, _context *context.Context) {
+func testUpdatedWorkItem(t *testing.T, _context *context.Context, bagIdentifier, etag string) {
 	params := url.Values{}
 	params.Set("item_action", constants.ActionIngest)
-	params.Set("object_identifier", UPDATED_BAG_IDENTIFIER)
-	params.Set("etag", UPDATED_BAG_ETAG)
+	params.Set("object_identifier", bagIdentifier)
+	params.Set("etag", etag)
 	params.Set("page", "1")
 	params.Set("per_page", "1")
 
@@ -60,7 +65,7 @@ func testUpdatedWorkItem(t *testing.T, _context *context.Context) {
 	require.Nil(t, resp.Error)
 	workItem := resp.WorkItem()
 
-	require.NotNil(t, workItem, "WorkItem is missing for %s", UPDATED_BAG_IDENTIFIER)
+	require.NotNil(t, workItem, "WorkItem is missing for %s", bagIdentifier)
 	assert.Equal(t, constants.StatusSuccess, workItem.Status)
 	assert.Equal(t, constants.StageCleanup, workItem.Stage)
 	assert.Equal(t, "Item was successfully ingested", workItem.Note)
@@ -87,6 +92,7 @@ func testUpdatedObject(t *testing.T, _context *context.Context) {
 	assert.Equal(t, 15, fileLevelEventCount)
 	assert.Empty(t, obj.FindEventsByType(constants.EventDeletion))
 	require.Equal(t, 12, len(obj.GenericFiles))
+	assert.Equal(t, constants.StorageStandard, obj.StorageOption)
 
 	testUpdatedFilesInPharos(t, _context, obj)
 }
@@ -98,6 +104,7 @@ func testUpdatedFilesInPharos(t *testing.T, _context *context.Context, obj *mode
 	// and fixity generation.
 	updatedFile := obj.FindGenericFile("data/datastream-DC")
 	require.NotNil(t, updatedFile)
+	assert.Equal(t, constants.StorageStandard, updatedFile.StorageOption)
 	assert.Equal(t, 2, len(updatedFile.FindEventsByType(constants.EventIngestion)))
 	assert.Equal(t, 2, len(updatedFile.FindEventsByType(constants.EventDigestCalculation)))
 	assert.Equal(t, 2, len(updatedFile.FindEventsByType(constants.EventReplication)))
@@ -121,13 +128,6 @@ func testUpdatedFilesInPharos(t *testing.T, _context *context.Context, obj *mode
 		assert.True(t, util.StringListContains(actualDigests, expectedDigest),
 			"Digest %s not found", expectedDigest)
 	}
-
-	// require.Equal(t, "44d85cf4810d6c6fe87750117633e461", updatedFile.Checksums[0].Digest)
-	// require.Equal(t, "248fac506a5c46b3c760312b99827b6fb5df4698d6cf9a9cdc4c54746728ab99",
-	//	updatedFile.Checksums[1].Digest)
-	// require.Equal(t, "7a31f705fc1a16571374406c5a9b7681", updatedFile.Checksums[2].Digest)
-	// require.Equal(t, "baf8752080187b1e401ae952047029ae4e16b5f54c5daf9d97bc0c7598772326",
-	//	updatedFile.Checksums[3].Digest)
 
 	testUpdatedFileInStorage(t, _context, updatedFile)
 
@@ -183,4 +183,99 @@ func testUpdatedFileInStorage(t *testing.T, _context *context.Context, gf *model
 		assert.Equal(t, gf.GetChecksumByAlgorithm(constants.AlgMd5).Digest, storedFile.Md5, idAndUuid)
 		assert.Equal(t, gf.GetChecksumByAlgorithm(constants.AlgSha256).Digest, storedFile.Sha256, idAndUuid)
 	}
+}
+
+// ---------------------------------------------------------------------------
+
+func testUpdatedGlacierObject(t *testing.T, _context *context.Context) {
+	resp := _context.PharosClient.IntellectualObjectGet(UPDATED_GLACIER_BAG_IDENTIFIER, true, true)
+	obj := resp.IntellectualObject()
+	require.NotNil(t, obj)
+
+	ingestEvents := obj.FindEventsByType(constants.EventIngestion)
+	fileLevelEventCount := 0
+	objectLevelEventCount := 0
+	for _, event := range ingestEvents {
+		if strings.HasPrefix(event.Detail, "Copied all files to perservation bucket") {
+			objectLevelEventCount++
+		} else if strings.HasPrefix(event.Detail, "Completed copy to") {
+			fileLevelEventCount++
+		}
+	}
+	assert.Equal(t, 2, objectLevelEventCount)
+	assert.Equal(t, 15, fileLevelEventCount) // What should this count be??
+	assert.Empty(t, obj.FindEventsByType(constants.EventDeletion))
+	require.Equal(t, 6, len(obj.GenericFiles))
+	assert.Equal(t, constants.StorageGlacierOH, obj.StorageOption)
+
+	testUpdatedGlacierFilesInPharos(t, _context, obj)
+}
+
+func testUpdatedGlacierFilesInPharos(t *testing.T, _context *context.Context, obj *models.IntellectualObject) {
+	// The updated file is different in the new version of the bag.
+	// We have to make sure we've stored this and have the new
+	// checksum attached to it. It should have two events for ingest,
+	// and fixity generation.
+	updatedFile := obj.FindGenericFile("data/datastream-DC")
+	require.NotNil(t, updatedFile)
+	assert.Equal(t, constants.StorageGlacierOH, updatedFile.StorageOption)
+	assert.Equal(t, 2, len(updatedFile.FindEventsByType(constants.EventIngestion)))
+	assert.Equal(t, 2, len(updatedFile.FindEventsByType(constants.EventDigestCalculation)))
+	assert.Equal(t, 0, len(updatedFile.FindEventsByType(constants.EventReplication)))
+	require.Equal(t, 4, len(updatedFile.Checksums))
+
+	actualDigests := []string{
+		updatedFile.Checksums[0].Digest,
+		updatedFile.Checksums[1].Digest,
+		updatedFile.Checksums[2].Digest,
+		updatedFile.Checksums[3].Digest,
+	}
+	expectedDigests := []string{
+		"44d85cf4810d6c6fe87750117633e461",
+		"248fac506a5c46b3c760312b99827b6fb5df4698d6cf9a9cdc4c54746728ab99",
+		"7a31f705fc1a16571374406c5a9b7681",
+		"baf8752080187b1e401ae952047029ae4e16b5f54c5daf9d97bc0c7598772326",
+	}
+
+	// Digests may be in unexpected order. Make sure they're all present.
+	for _, expectedDigest := range expectedDigests {
+		assert.True(t, util.StringListContains(actualDigests, expectedDigest),
+			"Digest %s not found", expectedDigest)
+	}
+
+	testUpdatedGlacierFileInStorage(t, _context, updatedFile)
+
+	otherUpdatedFile := obj.FindGenericFile("data/datastream-RELS-EXT")
+	require.NotNil(t, otherUpdatedFile)
+	// TODO: Test checksums for this
+	testUpdatedGlacierFileInStorage(t, _context, otherUpdatedFile)
+}
+
+func testUpdatedGlacierFileInStorage(t *testing.T, _context *context.Context, gf *models.GenericFile) {
+	client := network.NewS3Head(
+		os.Getenv("AWS_ACCESS_KEY_ID"),
+		os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		_context.Config.GlacierRegionOH,
+		_context.Config.GlacierBucketOH)
+
+	// This is the name of the file in S3/Glacier
+	uuid, err := gf.PreservationStorageFileName()
+	require.Nil(t, err, gf.Identifier)
+
+	// Make sure file is present and all metadata is set correctly.
+	idAndUuid := fmt.Sprintf("%s (%s)", gf.Identifier, uuid)
+	client.Head(uuid)
+	require.Empty(t, client.ErrorMessage, idAndUuid)
+	storedFile := client.StoredFile()
+	require.NotNil(t, storedFile, idAndUuid)
+
+	institution, _ := gf.InstitutionIdentifier()
+	timeSinceLastModified := time.Since(storedFile.LastModified)
+	assert.True(t, timeSinceLastModified < (10*time.Minute))
+	assert.EqualValues(t, gf.Size, storedFile.Size, idAndUuid)
+	assert.Equal(t, institution, storedFile.Institution, idAndUuid)
+	assert.Equal(t, gf.IntellectualObjectIdentifier, storedFile.BagName, idAndUuid)
+	assert.Equal(t, gf.OriginalPath(), storedFile.PathInBag, idAndUuid)
+	assert.Equal(t, gf.GetChecksumByAlgorithm(constants.AlgMd5).Digest, storedFile.Md5, idAndUuid)
+	assert.Equal(t, gf.GetChecksumByAlgorithm(constants.AlgSha256).Digest, storedFile.Sha256, idAndUuid)
 }
