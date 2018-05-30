d937d341cd10f92d8d86010f20aac416374c188f
A. Diamond <diamond@sheprador.com>
2018-05-30 14:20:14 -0400

    Working on mocks for S3 restore

--- network/s3_mock.go
+++ network/s3_mock.go
@@ -50,7 +50,49 @@ func S3HeadRestoreCompletedHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "")
 }
 
+func getRestoreHeaders() map[string]string {
+	return map[string]string{
+		"x-amz-id-2":       "GFihv3y6+kE7KG11GEkQhU7/2/cHR3Yb2fCb2S04nxI423Dqwg2XiQ0B/UZlzYQvPiBlZNRcovw=",
+		"x-amz-request-id": "9F341CD3C4BA79E0",
+		"Date":             "Wed, 30 May 2018 22:32:00 GMT",
+		"Content-Length":   "0",
+		"Server":           "AmazonS3",
+	}
+}
+
 // Handles a request to restore a Glacier object to S3.
 func S3RestoreHandler(w http.ResponseWriter, r *http.Request) {
+	for key, value := range getRestoreHeaders() {
+		w.Header().Set(key, value)
+	}
+	// Must return 202 to indicate the request was accepted
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprintln(w, "")
+}
 
+// Handles a request to restore a Glacier object to S3,
+// and replies with a 409 to say the restoration is
+// already in progress.
+// See https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPOSTrestore.html
+func S3RestoreInProgressHandler(w http.ResponseWriter, r *http.Request) {
+	for key, value := range getRestoreHeaders() {
+		w.Header().Set(key, value)
+	}
+	// Must return 409 to indicate the request conflicts
+	// with one already in progress
+	w.WriteHeader(http.StatusConflict)
+	fmt.Fprintln(w, "")
+}
+
+// Handles a request to restore a Glacier object to S3,
+// and replies with a 200 to say the restoration is
+// already completed (item already restored to active tier)
+func S3RestoreCompletedHandler(w http.ResponseWriter, r *http.Request) {
+	for key, value := range getRestoreHeaders() {
+		w.Header().Set(key, value)
+	}
+	// Must return 200 to indicate item has already
+	// been restored to S3
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "")
 }
