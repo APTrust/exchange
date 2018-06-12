d1da16080d977f3593f30e8d483cd175617aaa23
A. Diamond <diamond@sheprador.com>
2018-06-12 15:28:39 -0400

    More groundwork for Glacier restore tests

--- workers/apt_glacier_restore_init_test.go
+++ workers/apt_glacier_restore_init_test.go
@@ -1,19 +1,57 @@
 package workers_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/APTrust/exchange/constants"
+	"github.com/APTrust/exchange/models"
 	"github.com/APTrust/exchange/util/testutil"
 	"github.com/APTrust/exchange/workers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	//"net/http"
-	//"net/http/httptest"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+var workItemTestServer = httptest.NewServer(http.HandlerFunc(workItemGetHandler))
+var workItemStateTestServer = httptest.NewServer(http.HandlerFunc(workItemStateGetHandler))
 
 func getGlacierRestoreWorker(t *testing.T) *workers.APTGlacierRestoreInit {
 	_context, err := testutil.GetContext("integration.json")
 	require.Nil(t, err)
 	return workers.NewGlacierRestore(_context)
+}
+
+func getObjectWorkItem(id int, objectIdentifier string) *models.WorkItem {
+	return &models.WorkItem{
+		Id:                    id,
+		ObjectIdentifier:      objectIdentifier,
+		GenericFileIdentifier: "",
+		Name:             "glacier_bag.tar",
+		Bucket:           "aptrust.receiving.test.edu",
+		ETag:             "0000000000000000",
+		BagDate:          testutil.RandomDateTime(),
+		InstitutionId:    33,
+		User:             "frank.zappa@example.com",
+		Date:             testutil.RandomDateTime(),
+		Note:             "",
+		Action:           constants.ActionGlacierRestore,
+		Stage:            constants.StageRequested,
+		Status:           constants.StatusPending,
+		Outcome:          "",
+		Retry:            true,
+		Node:             "",
+		Pid:              0,
+		NeedsAdminReview: false,
+	}
+}
+
+func getFileWorkItem(id int, objectIdentifier, fileIdentifier string) *models.WorkItem {
+	workItem := getObjectWorkItem(id, objectIdentifier)
+	workItem.GenericFileIdentifier = fileIdentifier
+	return workItem
 }
 
 func TestNewGlacierRestore(t *testing.T) {
@@ -25,7 +63,18 @@ func TestNewGlacierRestore(t *testing.T) {
 }
 
 func TestGetGlacierRestoreState(t *testing.T) {
+	glacierRestore := getGlacierRestoreWorker(t)
+	require.NotNil(t, glacierRestore)
 
+	id := 1000
+	objIdentifier := "test.edu/glacier_bag"
+	workItem := getObjectWorkItem(id, objIdentifier)
+	nsqMessage := testutil.MakeNsqMessage(fmt.Sprintf("%d", id))
+
+	glacierRestore.Context.Config.PharosURL = workItemTestServer.URL
+	glacierRestoreState, err := glacierRestore.GetGlacierRestoreState(nsqMessage, workItem)
+	require.NotNil(t, glacierRestoreState)
+	require.Nil(t, err)
 }
 
 func TestHandleMessage(t *testing.T) {
@@ -114,3 +163,39 @@ func TestInitializeRetrieval(t *testing.T) {
 // Will need a customized Context object where URLs for NSQ,
 // Pharos, S3, and Glacier point to the mock services.
 // -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+// HTTP test handlers
+// -------------------------------------------------------------------------
+
+func getRequestData(r *http.Request) (map[string]interface{}, error) {
+	decoder := json.NewDecoder(r.Body)
+	decoder.UseNumber()
+	data := make(map[string]interface{})
+	err := decoder.Decode(&data)
+	return data, err
+}
+
+func workItemGetHandler(w http.ResponseWriter, r *http.Request) {
+	obj := testutil.MakeWorkItem()
+	objJson, _ := json.Marshal(obj)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(objJson))
+}
+
+func workItemStateGetHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := getRequestData(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding JSON data: %v", err)
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	obj := testutil.MakeWorkItemState()
+	obj.WorkItemId = data["id"].(int)
+	obj.Action = constants.ActionGlacierRestore
+	obj.State = ""
+
+	objJson, _ := json.Marshal(obj)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(objJson))
+}
