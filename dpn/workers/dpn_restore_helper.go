b1d3f6510f2a31502b63f87e73237fe0194c7303
A. Diamond <diamond@sheprador.com>
2018-09-11 11:29:05 -0400

    Started to move common restore code into DPNRestoreHelper

---
+++ dpn/workers/dpn_restore_helper.go
@@ -0,0 +1,184 @@
+package workers
+
+import (
+	"fmt"
+	"github.com/APTrust/exchange/constants"
+	"github.com/APTrust/exchange/context"
+	"github.com/APTrust/exchange/dpn/models"
+	"github.com/APTrust/exchange/dpn/network"
+	apt_models "github.com/APTrust/exchange/models"
+	"github.com/nsqio/go-nsq"
+	"strconv"
+	"strings"
+)
+
+type DPNRestoreHelper struct {
+	Manifest      *models.DPNRetrievalManifest
+	WorkSummary   *apt_models.WorkSummary
+	context       context.Context
+	dpnRestClient *network.DPNRestClient
+	summaryName   string
+	workItemId    int
+}
+
+func NewDPNRestoreHelper(message *nsq.Message, _context context.Context, dpnRestClient *network.DPNRestClient, action, summaryName string) (*DPNRestoreHelper, error) {
+	helper := &DPNRestoreHelper{
+		context:       _context,
+		dpnRestClient: dpnRestClient,
+		summaryName:   summaryName,
+	}
+	// action should be constants.ActionFixityCheck or constants.ActionRestore
+	err := helper.initManifest(message, action)
+	return helper, err
+}
+
+func (helper *DPNRestoreHelper) initManifest(message *nsq.Message, action string) error {
+	msgBody := strings.TrimSpace(string(message.Body))
+	helper.context.MessageLog.Info("NSQ Message body: '%s'", msgBody)
+	helper.Manifest = models.NewDPNRetrievalManifest(message)
+	helper.Manifest.TaskType = action
+	helper.Manifest.GlacierBucket = helper.context.Config.DPN.DPNPreservationBucket
+
+	// Set the WorkSummary to match the current action. E.g. If we're
+	// working on the GlacierRestore phase, we should record errors in
+	// the GlacierRestore WorkSummary
+	helper.WorkSummary = helper.Manifest.GetSummary(helper.summaryName)
+	if helper.WorkSummary == nil {
+		return fmt.Errorf("Manifest has no WorkSummary for %s", helper.summaryName)
+	}
+
+	var err error
+	helper.workItemId, err = strconv.Atoi(string(msgBody))
+	if err != nil || helper.workItemId == 0 {
+		helper.WorkSummary.AddError(
+			"Could not get DPNWorkItem Id from NSQ message body: %v", err)
+		return err
+	}
+
+	err = helper.getDPNWorkItem()
+	if err != nil {
+		return err
+	}
+
+	if helper.Manifest.DPNWorkItem.State != nil && *helper.Manifest.DPNWorkItem.State != "" {
+		restoredManifest, err := models.DPNRetrievalManifestFromJson(*helper.Manifest.DPNWorkItem.State)
+		if err != nil {
+			helper.context.MessageLog.Warning("Error restoring manifest state "+
+				"for DPNWorkItem %d: %v JSON:\n%s", helper.Manifest.DPNWorkItem.Id, err,
+				helper.Manifest.DPNWorkItem.Identifier, *helper.Manifest.DPNWorkItem.State)
+			helper.context.MessageLog.Warning("Starting with new manifest for "+
+				"for DPNWorkItem %d (%s)", helper.Manifest.DPNWorkItem.Id,
+				helper.Manifest.DPNWorkItem.Identifier)
+		} else {
+			restoredManifest.NsqMessage = helper.Manifest.NsqMessage
+			restoredManifest.DPNWorkItem = helper.Manifest.DPNWorkItem
+			helper.Manifest = restoredManifest
+		}
+	}
+
+	// This will be nil for new jobs, and should be non-nil if we're reattempting
+	// an existing job. DPN bag records are immutable, so we don't need to reload
+	// them each time we reattempt a task. (Unlike DPNWorkItem, which can be
+	// cancelled between attempts.)
+	if helper.Manifest.DPNBag == nil {
+		err = helper.getDPNBag()
+		if err != nil {
+			return err
+		}
+	}
+	if helper.Manifest.ExpectedFixityValue == "" {
+		err = helper.getBagDigest()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (helper *DPNRestoreHelper) getDPNWorkItem() error {
+	resp := helper.context.PharosClient.DPNWorkItemGet(helper.workItemId)
+	if resp.Error != nil {
+		helper.WorkSummary.AddError(
+			"Error getting DPNWorkItem %d from Pharos: %v",
+			helper.workItemId, resp.Error)
+		return resp.Error
+	}
+	dpnWorkItem := resp.DPNWorkItem()
+	if dpnWorkItem == nil {
+		errMessage := fmt.Sprintf("Pharos returned nil for WorkItem %d", helper.workItemId)
+		helper.WorkSummary.AddError(errMessage)
+		return fmt.Errorf(errMessage)
+	}
+
+	helper.Manifest.DPNWorkItem = dpnWorkItem
+	helper.Manifest.DPNWorkItem.SetNodeAndPid()
+	note := "Requesting Glacier restoration for fixity"
+	helper.Manifest.DPNWorkItem.Note = &note
+	return nil
+}
+
+func (helper *DPNRestoreHelper) getDPNBag() error {
+	// Get the DPN Bag from the DPN REST server.
+	resp := helper.dpnRestClient.DPNBagGet(helper.Manifest.DPNWorkItem.Identifier)
+	if resp.Error != nil {
+		errMsg := fmt.Sprintf("Error getting DPN bag %s from %s: %v",
+			helper.Manifest.DPNWorkItem.Identifier,
+			helper.context.Config.DPN.RestClient.LocalServiceURL, resp.Error)
+		helper.WorkSummary.AddError(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+	dpnBag := resp.Bag()
+	if dpnBag == nil {
+		errMsg := fmt.Sprintf("DPN REST server returned nil for bag %s",
+			helper.Manifest.DPNWorkItem.Identifier)
+		helper.WorkSummary.AddError(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+	helper.Manifest.DPNBag = dpnBag
+	return nil
+}
+
+func (helper *DPNRestoreHelper) getBagDigest() error {
+	resp := helper.dpnRestClient.DigestGet(helper.Manifest.DPNWorkItem.Identifier, constants.AlgSha256)
+	if resp.Error != nil {
+		errMsg := fmt.Sprintf("Error getting sha256 digest for DPN bag %s from %s: %v",
+			helper.Manifest.DPNWorkItem.Identifier,
+			helper.context.Config.DPN.RestClient.LocalServiceURL, resp.Error)
+		helper.WorkSummary.AddError(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+	digest := resp.Digest()
+	if digest == nil {
+		errMsg := fmt.Sprintf("DPN REST server returned nil digest for bag %s",
+			helper.Manifest.DPNWorkItem.Identifier)
+		helper.WorkSummary.AddError(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+	helper.Manifest.ExpectedFixityValue = digest.Value
+	return nil
+}
+
+func (helper *DPNRestoreHelper) SaveDPNWorkItem() {
+	jsonData, err := helper.Manifest.ToJson()
+	if err != nil {
+		msg := fmt.Sprintf("Could not marshal DPNRetrievalManifest "+
+			"for DPNWorkItem %d: %v", helper.Manifest.DPNWorkItem.Id, err)
+		helper.context.MessageLog.Error(msg)
+		note := "JSON serialization error while trying to save DPNWorkItemState."
+		helper.Manifest.DPNWorkItem.Note = &note
+	}
+
+	// Update the DPNWorkItem
+	helper.Manifest.DPNWorkItem.State = &jsonData
+	helper.Manifest.DPNWorkItem.Retry = !helper.WorkSummary.ErrorIsFatal
+
+	resp := helper.context.PharosClient.DPNWorkItemSave(helper.Manifest.DPNWorkItem)
+	if resp.Error != nil {
+		msg := fmt.Sprintf("Could not save DPNWorkItem %d "+
+			"for fixity on bag %s to Pharos: %v",
+			helper.Manifest.DPNWorkItem.Id, helper.Manifest.DPNWorkItem.Identifier, err)
+		helper.context.MessageLog.Error(msg)
+		helper.WorkSummary.AddError(msg)
+	}
+}
