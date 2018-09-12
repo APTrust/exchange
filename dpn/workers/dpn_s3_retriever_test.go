2562ce12a39827390675919815d52a0b8505fe0d
A. Diamond <diamond@sheprador.com>
2018-09-12 14:01:15 -0400

    Test DownloadFile

--- dpn/workers/dpn_s3_retriever_test.go
+++ dpn/workers/dpn_s3_retriever_test.go
@@ -11,13 +11,14 @@ import (
 	//	"github.com/APTrust/exchange/network"
 	"github.com/APTrust/exchange/util/testutil"
 	"github.com/nsqio/go-nsq"
-	//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	//	"io/ioutil"
 	//	"net/http"
 	//	"net/http/httptest"
 	//	"strings"
 	//	"sync"
+	"path/filepath"
 	"testing"
 	//	"time"
 )
@@ -56,11 +57,45 @@ func getDPNS3TestItems(t *testing.T) (*workers.DPNS3Retriever, *nsq.Message, *te
 }
 
 func TestNewDPNS3Retriever(t *testing.T) {
-
+	worker := getDPNS3TestWorker(t)
+	assert.NotNil(t, worker.Context)
+	assert.NotNil(t, worker.LocalDPNRestClient)
+	assert.NotNil(t, worker.FetchChannel)
+	assert.NotNil(t, worker.CleanupChannel)
+	assert.Nil(t, worker.PostTestChannel)
 }
 
 func TestDPNS3Retriever_DownloadFile(t *testing.T) {
+	if !testutil.CanTestS3() {
+		return
+	}
+	// Download a file that exists.
+	// This hack temporarily changes the restoration bucket
+	// and key to a bucket/key we know exists. The bucket
+	// aptrust.integration.test always contains the items
+	// in testdata/s3_bags/TestData.zip
+	worker, _, _, helper := getDPNS3TestItems(t)
+	worker.Context.Config.DPN.DPNRestorationBucket = "aptrust.integration.test"
+	helper.Manifest.DPNBag.UUID = "example.edu.tagsample_good"
+	helper.Manifest.DPNBag.Size = uint64(40960)
+	expectedLocalPath := filepath.Join(worker.Context.Config.DPN.DPNRestorationDirectory,
+		helper.Manifest.DPNBag.UUID+".tar")
+	worker.DownloadFile(helper)
+	assert.False(t, helper.WorkSummary.HasErrors())
+	assert.Equal(t, expectedLocalPath, helper.Manifest.LocalPath)
+	assert.True(t, helper.FileExistsAndIsComplete())
 
+	// Download a file that does not exist
+	worker, _, _, helper = getDPNS3TestItems(t)
+	worker.Context.Config.DPN.DPNRestorationBucket = "aptrust.integration.test"
+	helper.Manifest.DPNBag.UUID = "this_file_does_not_exist"
+	expectedLocalPath = filepath.Join(worker.Context.Config.DPN.DPNRestorationDirectory,
+		helper.Manifest.DPNBag.UUID+".tar")
+	worker.DownloadFile(helper)
+	assert.True(t, helper.WorkSummary.HasErrors())
+	assert.True(t, helper.WorkSummary.ErrorIsFatal)
+	assert.Equal(t, expectedLocalPath, helper.Manifest.LocalPath)
+	assert.False(t, helper.FileExistsAndIsComplete())
 }
 
 func TestDPNS3Retriever_FinishWithSuccess(t *testing.T) {
