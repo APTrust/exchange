a1452ae791749ed23f65fa43c6c98fb1f89b6868
A. Diamond <diamond@sheprador.com>
2019-10-03 16:13:22 -0400

    Changed magic numbers to HACK constants

--- network/s3_restore_test.go
+++ network/s3_restore_test.go
@@ -2,6 +2,7 @@ package network_test
 
 import (
 	// "fmt"
+	"github.com/APTrust/exchange/constants"
 	"github.com/APTrust/exchange/network"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -26,7 +27,7 @@ func getS3RestoreClient() *network.S3Restore {
 	//
 	// HACK: The AWS S3 client prepends the bucket name to the
 	// S3 URL before it makes a request. Our mock S3 service
-	// runs on 12.0.0.1. We have to remove the "127." prefix on
+	// runs on 127.0.0.1. We have to remove the "127." prefix on
 	// our mock service URL in the tests below so that when
 	// the AWS S3 client prepends it, it resolves to the correct
 	// IP address.
@@ -34,7 +35,7 @@ func getS3RestoreClient() *network.S3Restore {
 		accessKeyId,
 		secretKey,
 		"us-east-1",
-		"127",
+		constants.AWS_TEST_HACK_BUCKET_NAME,
 		"my-file.txt",
 		"Standard",
 		3)
@@ -44,7 +45,7 @@ func TestS3RestoreNormal(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(network.S3RestoreHandler))
 	defer testServer.Close()
 	restoreClient := getS3RestoreClient()
-	restoreClient.TestURL = strings.Replace(testServer.URL, "127.", "", 1)
+	restoreClient.TestURL = strings.Replace(testServer.URL, constants.AWS_TEST_HACK_IP_PREFIX, "", 1)
 	restoreClient.Restore()
 	require.Empty(t, restoreClient.ErrorMessage)
 	assert.False(t, restoreClient.RestoreAlreadyInProgress)
@@ -58,7 +59,7 @@ func TestS3RestoreInProgress(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(network.S3RestoreInProgressHandler))
 	defer testServer.Close()
 	restoreClient := getS3RestoreClient()
-	restoreClient.TestURL = strings.Replace(testServer.URL, "127.", "", 1)
+	restoreClient.TestURL = strings.Replace(testServer.URL, constants.AWS_TEST_HACK_IP_PREFIX, "", 1)
 	restoreClient.Restore()
 	require.Empty(t, restoreClient.ErrorMessage)
 	assert.True(t, restoreClient.RestoreAlreadyInProgress)
@@ -72,7 +73,7 @@ func TestS3RestoreCompleted(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(network.S3RestoreCompletedHandler))
 	defer testServer.Close()
 	restoreClient := getS3RestoreClient()
-	restoreClient.TestURL = strings.Replace(testServer.URL, "127.", "", 1)
+	restoreClient.TestURL = strings.Replace(testServer.URL, constants.AWS_TEST_HACK_IP_PREFIX, "", 1)
 	restoreClient.Restore()
 	require.Empty(t, restoreClient.ErrorMessage)
 	assert.False(t, restoreClient.RestoreAlreadyInProgress)
@@ -90,7 +91,7 @@ func TestS3RestoreRejected(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(network.S3RestoreRejectHandler))
 	defer testServer.Close()
 	restoreClient := getS3RestoreClient()
-	restoreClient.TestURL = strings.Replace(testServer.URL, "127.", "", 1)
+	restoreClient.TestURL = strings.Replace(testServer.URL, constants.AWS_TEST_HACK_IP_PREFIX, "", 1)
 	restoreClient.Restore()
 	require.Empty(t, restoreClient.ErrorMessage)
 	assert.False(t, restoreClient.RestoreAlreadyInProgress)
